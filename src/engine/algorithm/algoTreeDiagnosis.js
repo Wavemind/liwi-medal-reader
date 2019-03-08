156a23c9bae88558386e1c0e7060b15e97af2442
Quentin Girard <quentin.girard@wavemind.ch>
2019-03-08 15:23:40 +0100

    migrate constant

--- src/engine/algorithm/algoTreeDiagnosis.js
+++ src/engine/algorithm/algoTreeDiagnosis.js
@@ -1,6 +1,6 @@
 import reduce from 'lodash/reduce';
 import find from 'lodash/find';
-import {nodesType} from '../../utils/constants';
+import {nodesType} from '../../../frontend_service/constants';
 
 
 // Create the first batch from json based on triage priority
