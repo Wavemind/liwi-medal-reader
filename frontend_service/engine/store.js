fef71c8e6ac24cb6d8a38faa5b5bda449460a036
Quentin Girard <quentin.girard@wavemind.ch>
2019-03-08 16:32:02 +0100

    Move store

--- frontend_service/engine/store.js
+++ frontend_service/engine/store.js
@@ -1,10 +1,10 @@
-import {applyMiddleware, createStore} from 'redux';
-import {createEpicMiddleware} from 'redux-observable';
+import { applyMiddleware, createStore } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
 import thunk from 'redux-thunk';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/es/storage';
-import rootReducer from '../../frontend_service/engine/reducers';
-import rootEpic from '../../frontend_service/engine/middlewares/epics';
+import rootReducer from './reducers';
+import rootEpic from './middlewares/epics';
 
 import {
   persistReducer,
