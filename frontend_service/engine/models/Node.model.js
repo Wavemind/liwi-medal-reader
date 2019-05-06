f56b3112699be645fa0ef7aa7697cf0b0594102c
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-05-06 14:46:47 +0200

    model node

--- frontend_service/engine/models/Node.model.js
+++ frontend_service/engine/models/Node.model.js
@@ -1,20 +1,23 @@
 // @flow
 import { nodesType } from '../../constants';
+import { MedicalCaseModel } from './MedicalCase.model';
 
 const { ps, d, fd, m, q, t } = nodesType;
 
- interface NodeInterface {
+interface NodeInterface {
   id: number;
   type: ps | d | fd | m | q | t;
   reference: string;
+  medicalCase: MedicalCaseModel;
 }
 
 export class NodeModel implements NodeInterface {
   constructor(props) {
-    const { id = 0, type = '', reference = '' } = props;
+    const { id = 0, type = '', reference = '', medicalCase = {} } = props;
 
     this.id = id;
     this.type = type;
     this.reference = reference;
+    this.medicalCase = medicalCase;
   }
 }
