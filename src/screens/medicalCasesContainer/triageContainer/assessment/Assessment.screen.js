e3f3f4a822341b1995fa4f12b070210c2cc48b65
Quentin Girard <quentin.girard@wavemind.ch>
2019-06-17 17:15:48 +0200

    Add style for question in first assessment

--- src/screens/medicalCasesContainer/triageContainer/assessment/Assessment.screen.js
+++ src/screens/medicalCasesContainer/triageContainer/assessment/Assessment.screen.js
@@ -1,11 +1,12 @@
 // @flow
 
 import * as React from 'react';
-import { ScrollView } from 'react-native';
-import { View } from 'native-base';
+import { ScrollView, TouchableOpacity } from 'react-native';
+import { Button, List, ListItem, Text, View } from 'native-base';
 
-import { LiwiTitle2 } from '../../../template/layout';
-import i18n from '../../../utils/i18n';
+import { LiwiTitle2 } from '../../../../template/layout';
+import i18n from '../../../../utils/i18n';
+import { liwiColors } from '../../../../utils/constants';
 
 type Props = {};
 type State = {};
@@ -16,7 +17,52 @@ export default class Assessment extends React.Component<Props, State> {
     return (
       <ScrollView>
         <View padding-auto>
-          <LiwiTitle2 noBorder>{i18n.t('patient_list:search')}</LiwiTitle2>
+          <LiwiTitle2 noBorder>{i18n.t('assessment:title')}</LiwiTitle2>
+          <List block>
+            <ListItem
+              block
+              spaced
+              noBorder
+            >
+              <View flex-container-row style={{
+                flex:0.8,
+                borderRadius: 5,
+                borderWidth: 1,
+                padding: 15,
+                marginRight: 20,
+                borderColor: liwiColors.blackColor,
+              }}>
+                <Text>
+                  Chabat chabit ou les deux
+                </Text>
+              </View>
+              <View flex-container-row style={{
+                flex:0.2,
+                justifyContent: 'center',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}>
+                <TouchableOpacity style={{
+                  borderTopLeftRadius: 5,
+                  borderBottomLeftRadius: 5,
+                  borderWidth: 1,
+                  padding: 15,
+                  borderColor: liwiColors.blackColor,
+                  }}>
+                <Text>Oui</Text>
+              </TouchableOpacity>
+                <TouchableOpacity style={{
+                  borderTopRightRadius: 5,
+                  borderBottomRightRadius: 5,
+                  borderWidth: 1,
+                  padding: 15,
+                  borderColor: liwiColors.blackColor,
+                }}>
+                  <Text>Non</Text>
+                </TouchableOpacity>
+              </View>
+            </ListItem>
+          </List>
         </View>
       </ScrollView>
     );
