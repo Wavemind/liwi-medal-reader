7cb06d6f0b9758f4290992eea7c6c330c4777998
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-07-16 18:01:23 +0200

    add bottom menu

---
+++ src/engine/navigation/patientProfileMenu/index.js
@@ -0,0 +1,17 @@
+import TabsNavigation from './PatientProfileMenu.navigation';
+import { withSessions } from '../../../engine/contexts/Sessions.context';
+import { withApplication } from '../../../engine/contexts/Application.context';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (medicalCase, ownProps) => {
+  return { medicalCase };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withSessions(withApplication(TabsNavigation)));
