b9dbc3c1c0794896feee7bc9cc52fccfa5e4caf9
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-07-24 17:36:47 +0200

    fix drop down nav, fix questions fitler, work with nav

--- src/screens/medicalCasesContainer/consultationContainer/poct/index.js
+++
@@ -1,6 +0,0 @@
-import Poct from './Poct.screen';
-import { withSessions } from '../../../../engine/contexts/Sessions.context';
-import { withApplication } from '../../../../engine/contexts/Application.context';
-
-
-export default withSessions(withApplication(Poct));
