0b80d86500db9f7f4c0292879405f6e81acb9530
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-08-07 00:17:51 +0200

    files changed

---
+++ src/screens/medicalCasesContainer/tests/Tests.style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+});
