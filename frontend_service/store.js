ce8000dd961a023d79141290f98cb88d46b30ec1
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-08-29 15:41:36 +0200

    refractor folders structure for epics

--- frontend_service/store.js
+++ frontend_service/store.js
@@ -5,7 +5,7 @@ import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import AsyncStorage from '@react-native-community/async-storage';
 import { persistReducer, persistStore } from 'redux-persist';
 import rootReducer from './reducers';
-import rootEpic from './middlewares/epics';
+import rootEpic from './algorithm/epics';
 
 
 const persistConfig = {
