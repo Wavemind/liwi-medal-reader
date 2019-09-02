570989480d4ac5ec6122059d1d80e91158b3eb73
Quentin Girard <quentin.girard@wavemind.ch>
2019-09-02 16:55:04 +0200

    Remove unused lib

--- src/screens/medicalCasesContainer/triageContainer/chronicalConditions/ChronicalConditions.screen.js
+++ src/screens/medicalCasesContainer/triageContainer/chronicalConditions/ChronicalConditions.screen.js
@@ -3,33 +3,25 @@
 import * as React from 'react';
 import type { NavigationScreenProps } from 'react-navigation';
 import type { StateApplicationContext } from '../../../../engine/contexts/Application.context';
-import QuestionList from '../../../../components/Triage/QuestionList';
 import { categories } from '../../../../../frontend_service/constants';
+import QuestionList from '../../../../components/Triage/QuestionList';
 
 type Props = NavigationScreenProps & {};
 type State = StateApplicationContext & {};
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class ChronicalConditions extends React.Component<Props, State> {
-  state: {
-    questions: []
-  };
-
-  // Fetch first look assessment questions and order it
-  componentWillMount() {
+  render() {
     const { medicalCase } = this.props;
     let questions = [];
-    const orders = medicalCase.triage_orders[categories.chronicalCondition];
+    const orderedQuestions = medicalCase.triage.orders[categories.chronicalCondition];
 
-    orders.map((order) => {
-      questions.push(medicalCase.nodes[order]);
+    orderedQuestions.map((orderedQuestion) => {
+      let question = medicalCase.nodes[orderedQuestion];
+      if (question.isDisplayedInTriage(medicalCase)) {
+        questions.push(question);
+      }
     });
-
-    this.setState({questions});
-  }
-
-  render() {
-    const { questions } = this.state;
 
     return (
       <QuestionList questions={questions} />
