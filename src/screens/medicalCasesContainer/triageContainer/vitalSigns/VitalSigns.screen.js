5a4804d6f8acc6712d74a9a230a850d971f3182a
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-09-11 16:27:53 +0200

    fix bug from merge

--- src/screens/medicalCasesContainer/triageContainer/vitalSigns/VitalSigns.screen.js
+++ src/screens/medicalCasesContainer/triageContainer/vitalSigns/VitalSigns.screen.js
@@ -5,6 +5,7 @@ import type { NavigationScreenProps } from 'react-navigation';
 import type { StateApplicationContext } from '../../../../engine/contexts/Application.context';
 import { categories } from '../../../../../frontend_service/constants';
 import QuestionList from '../../../../components/Triage/QuestionList';
+import NavigationTriage from '../../../../components/Triage/NavigationTriage';
 
 type Props = NavigationScreenProps & {};
 type State = StateApplicationContext & {};
@@ -24,7 +25,10 @@ export default class VitalSigns extends React.Component<Props, State> {
     });
 
     return (
-      <QuestionList questions={questions} />
+      <React.Fragment>
+        <QuestionList questions={questions} />
+        <NavigationTriage />
+      </React.Fragment>
     );
   }
 }
