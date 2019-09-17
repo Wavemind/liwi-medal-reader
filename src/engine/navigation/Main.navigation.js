ddf062dd2c305f46f020e57530f5f01e2ffa4c5d
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-09-17 13:09:32 +0200

    fix bug and files name

--- src/engine/navigation/Main.navigation.js
+++ src/engine/navigation/Main.navigation.js
@@ -23,7 +23,7 @@ import PatientSummaryMenu from './patientSummaryMenu';
 import MedicalCaseSummary from '../../screens/medicalCasesContainer/medicalCaseSummary';
 import MedicalCaseList from '../../screens/medicalCasesContainer/medicalCaseList';
 import Tests from '../../screens/medicalCasesContainer/tests';
-import DiagnosticsStrategy from '../../screens/medicalCasesContainer/DiagnosticsStrategyContainer/DiagnosticsStrategy';
+import DiagnosticsStrategy from '../../screens/medicalCasesContainer/diagnosticsStrategyContainer/diagnosticsStrategy';
 import { medicalCaseStatus } from '../../../frontend_service/constants';
 
 // We need to use i18n directly because we cant be connect to context
