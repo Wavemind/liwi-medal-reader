5641420fe396a026f7161e4333ea2857846474a4
Quentin Girard <quentin.girard@wavemind.ch>
2019-10-08 10:57:06 +0200

    Fix PR

--- src/components/QuestionsContainer/DisplaysContainer/Date/Date.style.js
+++ src/components/QuestionsContainer/DisplaysContainer/Date/Date.style.js
@@ -1,29 +1,7 @@
 import { StyleSheet } from 'react-native';
-import {liwiColors} from '../../../../utils/constants';
+import { liwiColors } from '../../../../utils/constants';
 
 export const styles = StyleSheet.create({
-  icon: {
-    fontSize: 25,
-    color: liwiColors.redColor,
-    marginRight: 8,
-  },
-
-  view: {
-    flexDirection: 'row',
-    paddingBottom: 5
-  },
-
-  textWithoutIcon: {
-    marginTop: 5,
-  },
-
-  textWithIcon: {
-    marginTop: 5,
-    marginLeft: 20,
-  },
-
-  form: {
-    marginTop: 20,
-    marginBottom: 20,
-  },
+  textColor: { color: liwiColors.blackColor, padding: 20 },
+  placeholder: { color: liwiColors.darkerGreyColor },
 });
