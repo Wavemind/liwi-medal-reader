8c9e43f007daefdf98b25716aba1ce267ea292cd
Alain Fresco <alain.fresco@gmail.com>
2019-11-12 15:10:59 +0100

    fix fileName

--- src/components/Consultation/QuestionsPerChiefComplaint/QuestionsPerChiefComplaint.js
+++ src/components/Consultation/QuestionsPerChiefComplaint/QuestionsPerChiefComplaint.js
@@ -28,9 +28,7 @@ export default class QuestionsPerChiefComplaint extends React.Component<Props,
 
     let questionsPerChiefComplaints = {};
     chiefComplaints.map((chiefComplaint) => {
-      if (
-        chiefComplaint.answer === Number(Object.keys(chiefComplaint.answers)[0])
-      ) {
+      if (chiefComplaint.answer === Number(Object.keys(chiefComplaint.answers)[0])) {
         questionsPerChiefComplaints[chiefComplaint.id] = {
           id: chiefComplaint.id,
           title: chiefComplaint.label,
--- src/screens/medicalCasesContainer/diagnosticsStrategyContainer/diagnosticsStrategy/DiagnosticsStrategy.screen.js
+++ src/screens/medicalCasesContainer/diagnosticsStrategyContainer/diagnosticsStrategy/DiagnosticsStrategy.screen.js
@@ -3,8 +3,8 @@
 import { NavigationScreenProps } from 'react-navigation';
 import React, { Component } from 'react';
 import { ScrollView, View } from 'react-native';
-import HealthCaresQuestions from '../HealthCaresQuestions';
-import HealthCares from '../HealthCares';
+import HealthCaresQuestions from '../healthCaresQuestions';
+import HealthCares from '../healthCares';
 import { styles } from './DiagnosticsStrategy.style';
 import Stepper from '../../../../components/Stepper';
 import FinalDiagnosticsList from '../../../../components/FinalDiagnosticsList';
