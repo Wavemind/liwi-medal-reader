80cd1285150eec3026b23572115af42cd55ef4dc
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-11-25 12:26:18 +0100

    improve navigation persist state

--- frontend_service/constants.js
+++ frontend_service/constants.js
@@ -2,6 +2,8 @@ export const host = 'http://liwi.wavelab.top/api/v1/';
 export const hostDataServer = 'https://liwi-main-data.herokuapp.com/api/';
 
 export const navigationStateKey = 'navigationState';
+export const alreadyLaunchedStateKey = 'alreadyLaunched';
+export const appInBackgroundStateKey = 'appBackground';
 // Hash used to encrypt local password
 export const saltHash = 'x9gKs?RBf*96RK2DAM+&$CYv7A3Gjp=?X&RBLS%9KeL8Q3dSGjUzL_?2Vye3';
 
