846ec3eb06f89d1c3f173d53aa1c729f906bac17
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-11-28 15:37:28 +0100

    recursif questions from qs into healthcare

--- frontend_service/engine/models/HealthCares.model.js
+++ frontend_service/engine/models/HealthCares.model.js
@@ -16,15 +16,23 @@ export class HealthCaresModel extends NodeModel implements HealthCaresInterface 
     this.label = label;
   }
 
+  getRecursifQuestionsInQs = (state$, questions, node) => {
+    Object.keys(node.instances).map((d) => {
+      if (state$.nodes[d].type === nodesType.questionsSequence) {
+        this.getRecursifQuestionsInQs(state$, questions, state$.nodes[d]);
+      } else {
+        questions[d] = state$.nodes[d];
+      }
+    });
+  };
+
   getQuestions(instanceHealthcare) {
     const state$ = store.getState();
     let questions = {};
     instanceHealthcare.top_conditions.map((tp) => {
       let node = state$.nodes[tp.first_node_id];
       if (node.type === nodesType.questionsSequence) {
-        if (node.answer === null) {
-          Object.keys(node.instances).map((d) => (questions[d] = state$.nodes[d]));
-        }
+        this.getRecursifQuestionsInQs(state$, questions, node);
       } else {
         questions[tp.first_node_id] = node;
       }
