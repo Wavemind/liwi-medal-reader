cd36c0e398e80067a24c0cf392fc220496fa6513
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-12-04 15:37:55 +0100

    scu slitttings drawer components not finish

--- src/engine/navigation/drawer/Drawer.item.navigation.js
+++ src/engine/navigation/drawer/Drawer.item.navigation.js
@@ -1,8 +1,10 @@
+// @Flow
 import React, { Component } from 'react';
 import { Button, Icon, Text } from 'native-base';
 import { styles } from './Drawer.style';
 import { TouchableOpacity, View } from 'react-native';
 import { liwiColors } from '../../../utils/constants';
+import { withNavigation } from 'react-navigation';
 
 export class CategorieButton extends Component<{ t: any, r: any, navigation: any }> {
   render() {
@@ -19,10 +21,8 @@ export class CategorieButton extends Component<{ t: any, r: any, navigation: any
 
 export class ItemButton extends Component<{ t: any, r: any, navigation: any }> {
   shouldComponentUpdate(nextProps) {
-    return true;
-
-    console.log(nextProps.r.params?.initialPage, this.props.r.params?.initialPage, nextProps.r.params?.routeName === this.props.name);
-    return nextProps.r.params?.initialPage !== this.props.r.params?.initialPage && nextProps.r.params?.routeName === this.props.name;
+    let { r } = this.props;
+    return r?.params?.initialPage !== nextProps.r?.params?.initialPage || nextProps.r?.routeName === r?.routeName;
   }
 
   render() {
@@ -42,11 +42,119 @@ export class ItemButton extends Component<{ t: any, r: any, navigation: any }> {
     );
   }
 }
+export const HeaderButtonsDrawer = withNavigation(
+  class Header extends React.Component {
+    shouldComponentUpdate(nextprops) {
+      return this.props?.r?.routeName !== nextprops?.r?.routeName;
+    }
+
+    render() {
+      const { r, navigation } = this.props;
+      console.log('render  HeaderButtonsDrawer');
+
+      return (
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', backgroundColor: liwiColors.redColor }}>
+          <Button
+            transparent
+            style={{
+              alignSelf: 'flex-start',
+            }}
+            homeDrawer
+            onPress={() => {
+              if (r.routeName === 'Home') {
+                navigation.toggleDrawer();
+              }
+              navigation.navigate('Home');
+            }}
+          >
+            <Icon style={styles.iconTop} dark type="FontAwesome" name="home" />
+          </Button>
+          <Button
+            transparent
+            style={{
+              alignSelf: 'flex-end',
+            }}
+            homeDrawer
+            onPress={() => navigation.navigate('Emergency')}
+          >
+            <Icon style={styles.iconTop} dark type="FontAwesome5" name="plus-square" />
+          </Button>
+          <Button
+            transparent
+            style={{
+              alignSelf: 'flex-end',
+            }}
+            homeDrawer
+            onPress={() => {
+              navigation.toggleDrawer();
+            }}
+          >
+            <Icon style={styles.iconTop} dark type="AntDesign" name="close" />
+          </Button>
+        </View>
+      );
+    }
+  }
+);
+
+export const BottomButtonsDrawer = withNavigation(
+  class BottomButtonsDrawerClasse extends React.Component {
+    shouldComponentUpdate(nextProps) {
+      return nextProps?.medicalCase?.patient?.id !== this.props?.medicalCase?.patient?.id;
+    }
+
+    render() {
+      const { navigation, medicalCase } = this.props;
+      console.log('render  BottomButtonsDrawer');
+      return (
+        <View style={styles.bottom}>
+          <Button
+            marginIcon
+            style={styles.bottomStyle}
+            onPress={() =>
+              navigation.navigate('Summary', {
+                id: medicalCase.patient.id,
+                defaultTab: 0,
+              })
+            }
+          >
+            <Icon style={{ fontSize: 40 }} drawerBottomIcon type="FontAwesome5" name="file-medical" />
+            <Text style={styles.textBottom}>Current Summary</Text>
+          </Button>
+        </View>
+      );
+    }
+  }
+);
 
 export class PathBar extends Component<{ active: any, routeName: string, navigation: Object, initialPage: string }> {
+  shouldComponentUpdate(nextProps): boolean {
+    let { r, routeName } = this.props;
+    console.log(nextProps.r?.routeName, routeName, r?.params?.initialPage, nextProps.r?.params?.initialPage);
+
+    // Juste arrive here
+    if (nextProps.r?.routeName === routeName && r?.routeName !== routeName) {
+      return true;
+    }
+
+    // Route we want and only initialPage not the same
+    if (nextProps.r?.routeName === routeName && r?.params?.initialPage !== nextProps.r?.params?.initialPage) {
+      return true;
+    }
+
+    if (routeName === r.routeName && nextProps.r?.routeName !== r?.routeName) {
+      return true;
+    }
+
+    return false;
+  }
+
   render() {
-    let { r, routeName, navigation, initialPage, navigate } = this.props;
+    let { r, routeName, initialPage, navigate } = this.props;
     let active = r?.params?.initialPage >= initialPage && r.routeName === routeName;
+
+    console.log('render  PathBar');
+
     return (
       <TouchableOpacity onPress={() => navigate(routeName, initialPage)} style={{ marginTop: -2, marginBottom: -3, flex: 1, flexDirection: 'row' }}>
         <View
--- src/engine/navigation/drawer/Drawer.navigation.js
+++ src/engine/navigation/drawer/Drawer.navigation.js
@@ -9,8 +9,9 @@ import type { StateApplicationContext } from '../../contexts/Application.context
 import NavigationService from '../Navigation.service';
 import { liwiColors } from '../../../utils/constants';
 import { medicalCaseStatus } from '../../../../frontend_service/constants';
-import { CategorieButton, ItemButton, PathBar } from './Drawer.item.navigation';
+import { BottomButtonsDrawer, CategorieButton, HeaderButtonsDrawer, ItemButton, PathBar } from './Drawer.item.navigation';
 import { Toaster } from '../../../utils/CustomToast';
+import { renderingDrawerItems } from './Drawer.constants';
 
 type Props = NavigationScreenProps & {};
 
@@ -40,17 +41,6 @@ export default class Drawer extends Component<Props, State> {
     const { navigation } = this.props;
     navigation.navigate(path);
   };
-
-  shouldComponentUpdate(nextProps: Readonly<P>): boolean {
-    const { props } = this;
-    return true;
-    let b =
-      nextProps.medicalCase.id !== props.medicalCase.id ||
-      nextProps.medicalCase.isCreating !== props.medicalCase.isCreating ||
-      Object.compare(nextProps.navigation, props.navigation);
-
-    return b;
-  }
 
   render() {
     const {
@@ -83,156 +73,16 @@ export default class Drawer extends Component<Props, State> {
       }[key];
     };
 
-    // routing render by objet, more clean
-    const renderingDrawerItems = [
-      {
-        type: 'categorie',
-        initialPage: 0,
-        name: 'Triage',
-        routeName: 'Triage',
-        t: t('menu:triage'),
-      },
-      {
-        type: 'path',
-        initialPage: 0,
-        routeName: 'Triage',
-      },
-      {
-        type: 'item',
-        initialPage: 0,
-        name: 'Triage',
-        routeName: 'Triage',
-        t: t('menu:first_look_assessments'),
-      },
-      {
-        type: 'path',
-        initialPage: 1,
-        routeName: 'Triage',
-      },
-      {
-        type: 'item',
-        initialPage: 1,
-        name: 'Triage',
-        routeName: 'Triage',
-        t: t('menu:chief_complaints'),
-      },
-      {
-        type: 'path',
-        initialPage: 2,
-        routeName: 'Triage',
-      },
-      {
-        type: 'item',
-        initialPage: 2,
-        name: 'Triage',
-        routeName: 'Triage',
-        t: t('menu:vital_signs'),
-      },
-      {
-        type: 'categorie',
-        initialPage: 0,
-        name: 'Consultation',
-        routeName: 'Consultation',
-        t: t('menu:consultation'),
-      },
-      {
-        type: 'path',
-        initialPage: 0,
-        routeName: 'Consultation',
-      },
-      {
-        type: 'item',
-        initialPage: 0,
-        name: 'Consultation',
-        routeName: 'Consultation',
-        t: t('menu:medical_history'),
-      },
-      {
-        type: 'path',
-        initialPage: 1,
-        routeName: 'Consultation',
-      },
-      {
-        type: 'item',
-        initialPage: 1,
-        name: 'Consultation',
-        routeName: 'Consultation',
-        t: t('menu:physical_exam'),
-      },
-      {
-        type: 'categorie',
-        initialPage: 0,
-        name: 'Tests',
-        routeName: 'Tests',
-        t: t('menu:tests'),
-      },
-      {
-        type: 'categorie',
-        initialPage: 0,
-        name: 'DiagnosticsStrategy',
-        routeName: 'DiagnosticsStrategy',
-        t: t('menu:strategy'),
-      },
-    ];
+    const renderDrawerButtons = (
+      <View style={[styles.top, { opacity: areMedicalCaseInredux ? 1 : 0.3 }]}>{renderingDrawerItems.map((routeur) => enumRender(routeur))}</View>
+    );
 
     return (
       <View style={[styles.columns, { width: drawerWidth, padding: 0 }]}>
         <View style={styles.tools}>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', backgroundColor: liwiColors.redColor }}>
-            <Button
-              transparent
-              style={{
-                alignSelf: 'flex-start',
-              }}
-              homeDrawer
-              onPress={() => {
-                if (r.routeName === 'Home') {
-                  navigation.toggleDrawer();
-                }
-                navigation.navigate('Home');
-              }}
-            >
-              <Icon style={styles.iconTop} dark type="FontAwesome" name="home" />
-            </Button>
-            <Button
-              transparent
-              style={{
-                alignSelf: 'flex-end',
-              }}
-              homeDrawer
-              onPress={() => navigation.navigate('Emergency')}
-            >
-              <Icon style={styles.iconTop} dark type="FontAwesome5" name="plus-square" />
-            </Button>
-            <Button
-              transparent
-              style={{
-                alignSelf: 'flex-end',
-              }}
-              homeDrawer
-              onPress={() => {
-                navigation.toggleDrawer();
-              }}
-            >
-              <Icon style={styles.iconTop} dark type="AntDesign" name="close" />
-            </Button>
-          </View>
-          <View style={[styles.top, { opacity: areMedicalCaseInredux ? 1 : 0.3 }]}>{renderingDrawerItems.map((routeur) => enumRender(routeur))}</View>
-          <View style={styles.bottom}>
-            <Button
-              marginIcon
-              style={styles.bottomStyle}
-              onPress={() =>
-                navigation.navigate('Summary', {
-                  id: medicalCase.patient.id,
-                  defaultTab: 0,
-                })
-              }
-            >
-              <Icon style={{ fontSize: 40 }} drawerBottomIcon type="FontAwesome5" name="file-medical" />
-              <Text style={styles.textBottom}>Current Summary</Text>
-            </Button>
-          </View>
+          <HeaderButtonsDrawer r={r} />
+          {renderDrawerButtons}
+          <BottomButtonsDrawer medicalCase={medicalCase} />
         </View>
       </View>
     );
