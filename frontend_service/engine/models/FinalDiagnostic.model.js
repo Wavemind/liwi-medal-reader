bb9ed4a01965ee446cf2bafe678e8d312f90e254
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2019-12-16 12:01:49 +0100

    refractor getstatusofdd into model

--- frontend_service/engine/models/FinalDiagnostic.model.js
+++ frontend_service/engine/models/FinalDiagnostic.model.js
@@ -2,10 +2,11 @@
 
 import { NodeModel } from './Node.model';
 import { RequirementNodeModel } from './RequirementNodeModel';
-import { calculateCondition } from '../../algorithm/algoConditionsHelpers';
+import { calculateCondition, comparingTopConditions, reduceConditionArrayBoolean } from '../../algorithm/algoConditionsHelpers';
 import { store } from '../../store';
 import { nodesType } from '../../constants';
-import { getStatusOfDD } from '../../algorithm/algoTreeDiagnosis';
+import find from 'lodash/find';
+import * as _ from 'lodash';
 
 interface FinalDiagnosticInterface {}
 
@@ -41,6 +42,107 @@ export class FinalDiagnosticModel extends NodeModel implements FinalDiagnosticIn
   }
 
   /**
+   * 1. Check the current status of this instance
+   * 2. Depending the result go recursive OR calculate final path DD
+   *
+   * @params state$: All the state of the reducer
+   * @params instance: The instance currently working
+   * @params dd: The Final Diagnostics we want to get the status
+   *
+   * @return boolean: return the status of the path
+   *      true = can reach the end
+   *      null = Still possible but not yet
+   *      false = can't access the end anymore
+   */
+  recursiveNodeDd = (state$, instance, dd) => {
+    /**
+     * Initial Var
+     */
+    let currentNode = state$.nodes[instance.id];
+    let instanceConditionValue = find(currentNode.dd, (p) => p.id === dd.diagnostic_id).conditionValue;
+
+    /**
+     * Get the condition of the instance link
+     * Do not change to this.calculateCondtion -> infinite loop
+     */
+    let instanceCondition = calculateCondition(instance);
+
+    // The condition path is not answered
+    // Wait on user
+    if (currentNode.answer === null && instanceCondition === true) {
+      return null;
+    }
+
+    // The condition path is not respected so we cant go deeper
+    if (instanceCondition === false && instanceConditionValue === false) {
+      return false;
+    }
+    // The condition path is not answered
+    // Wait on user
+    if (instanceCondition === null) {
+      return null;
+    }
+    // Remove path other dd
+    let childrenWithoutOtherDd = instance.children.filter((id) => {
+      if (state$.nodes[id].type === nodesType.finalDiagnostic && state$.nodes[id].id !== dd.id) {
+        return false;
+      }
+      return true;
+    });
+
+    let recursif = childrenWithoutOtherDd.map((childId) => {
+      let child = state$.nodes[childId];
+      // If this is not the final DD we calculate the conditonValue of the child
+      if (child.type === nodesType.question || child.type === nodesType.questionsSequence) {
+        return this.recursiveNodeDd(state$, state$.diagnostics[dd.diagnostic_id].instances[child.id], dd);
+      } else if (child.id === dd.id && child.type === nodesType.finalDiagnostic) {
+        let top_conditions = _.filter(dd.top_conditions, (top_condition) => top_condition.first_node_id === instance.id);
+        // We get the condition of the final link
+        let arrayBoolean = top_conditions.map((condition) => {
+          return comparingTopConditions(dd, condition);
+        });
+        // calcule final path
+        let r = reduceConditionArrayBoolean(arrayBoolean);
+        return r;
+      }
+    });
+
+    return reduceConditionArrayBoolean(recursif);
+  };
+
+  /**
+   * 1. Get top level node instance
+   * 2.
+   * 2. Depending the result go recursive OR calculate final path DD
+   *
+   * @params state$: All the state of the reducer
+   * @params dd: The Final Diagnostics we want to get the status
+   *
+   * @return boolean: return the status of the DD
+   *      true = can reach the end
+   *      null = Still possible but not yet
+   *      false = can't access the end anymore
+   */
+  getStatusOfDD = (state$, dd) => {
+    let topLevelNodes = [];
+    let instancesOfDiagnosticByDd = state$.diagnostics[dd.diagnostic_id].instances;
+    // Set top Level Nodes
+    Object.keys(instancesOfDiagnosticByDd).map((instanceId) => {
+      // Is top level nodes
+      const topLevelNode = instancesOfDiagnosticByDd[instanceId].top_conditions.length === 0;
+      // Is not related top treatment or management
+      const isNotRelatedToTreatmentManagement = instancesOfDiagnosticByDd[instanceId].final_diagnostic_id === null;
+      if (topLevelNode && isNotRelatedToTreatmentManagement) {
+        topLevelNodes.push(instancesOfDiagnosticByDd[instanceId]);
+      }
+    });
+
+    let allNodesAnsweredInDd = topLevelNodes.map((topNode) => this.recursiveNodeDd(state$, topNode, dd));
+
+    return reduceConditionArrayBoolean(allNodesAnsweredInDd);
+  };
+
+  /**
    * Calculate condition to display a final diagnostic
    * Verify if the final diagnostic excluded an another one
    */
@@ -56,7 +158,7 @@ export class FinalDiagnosticModel extends NodeModel implements FinalDiagnosticIn
     });
 
     // Return the status of this dd
-    let statusOfDD = getStatusOfDD(state$, this);
+    let statusOfDD = this.getStatusOfDD(state$, this);
 
     // If this FD can be excluded by other high-priority FD
     if (this.excluded_by_final_diagnostics !== null) {
