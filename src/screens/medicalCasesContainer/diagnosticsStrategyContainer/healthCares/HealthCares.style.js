ae934c23d68326ed1a19608535832d25e33ee90d
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2020-01-14 16:37:08 +0100

    change style

--- src/screens/medicalCasesContainer/diagnosticsStrategyContainer/healthCares/HealthCares.style.js
+++ src/screens/medicalCasesContainer/diagnosticsStrategyContainer/healthCares/HealthCares.style.js
@@ -2,18 +2,18 @@ import { StyleSheet } from 'react-native';
 import { liwiColors } from '../../../../utils/constants';
 
 export const styles = StyleSheet.create({
-         spaceText: {
-           marginTop: 20,
-           marginBottom: 20,
-           marginLeft: 20,
-           fontWeight: 'bold',
-         },
-         blocManagement: { borderBottomWidth: 0.5, borderColor: liwiColors.redColor },
-         desc: {
-           marginBottom: 20,
-           marginLeft: 40,
-         },
-         padded: {
-           padding: 20,
-         },
-       });
+  spaceText: {
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 20,
+    fontWeight: 'bold',
+  },
+  blocManagement: { borderBottomWidth: 0.5, borderColor: liwiColors.redColor },
+  desc: {
+    marginBottom: 20,
+    marginLeft: 40,
+  },
+  padded: {
+    padding: 20,
+  },
+});
