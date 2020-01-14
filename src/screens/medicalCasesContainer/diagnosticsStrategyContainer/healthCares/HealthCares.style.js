925a55ce1ebf7145144fdc00eda12ffdba0f1bfe
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2020-01-14 15:59:45 +0100

    fix management style

--- src/screens/medicalCasesContainer/diagnosticsStrategyContainer/healthCares/HealthCares.style.js
+++ src/screens/medicalCasesContainer/diagnosticsStrategyContainer/healthCares/HealthCares.style.js
@@ -1,15 +1,19 @@
 import { StyleSheet } from 'react-native';
+import { liwiColors } from '../../../../utils/constants';
 
 export const styles = StyleSheet.create({
-  spaceText: {
-    marginBottom: 20,
-    marginLeft:20,
-  },
-  desc: {
-    marginBottom: 10,
-    marginLeft:40,
-  },
-  padded: {
-    padding: 20,
-  },
-});
+         spaceText: {
+           marginTop: 20,
+           marginBottom: 20,
+           marginLeft: 20,
+           fontWeight: 'bold',
+         },
+         blocManagement: { borderBottomWidth: 0.5, borderColor: liwiColors.redColor },
+         desc: {
+           marginBottom: 20,
+           marginLeft: 40,
+         },
+         padded: {
+           padding: 20,
+         },
+       });
