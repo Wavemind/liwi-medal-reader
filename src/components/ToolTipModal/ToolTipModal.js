75e8c43940e322ad2540bbc72b9aba8ec1b79167
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2020-02-13 15:57:20 +0100

    remove unsued var

--- src/components/ToolTipModal/ToolTipModal.js
+++ src/components/ToolTipModal/ToolTipModal.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Image, ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import { Button, Icon, Text, View } from 'native-base';
 import { styles } from './ToolTipModal.style';
 import Tooltip from '../Tooltip/tooltip';
