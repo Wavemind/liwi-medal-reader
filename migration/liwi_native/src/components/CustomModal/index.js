d22888d410609cd749de0f3fb5b16d9d46306193
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2020-02-13 09:47:22 +0100

    delete old file
    Former-commit-id: 1b6ac9f3e1b88419b642db58075d17164538b157

--- migration/liwi_native/src/components/CustomModal/index.js
+++
@@ -1,5 +0,0 @@
-import CustomModal from './CustomModal';
-import { withSessions } from '../../engine/contexts/Sessions.context';
-import { withApplication } from '../../engine/contexts/Application.context';
-
-export default withSessions(withApplication(CustomModal));
