afd79375c736bd6a03bf08d0441fabd38e10ede0
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2020-02-13 14:24:25 +0100

    clean and ordering clean test
    Former-commit-id: 34922a7cc81d5361919a996e5875fd6278733b28

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,39 +1,15 @@
 /* eslint-env detox/detox, mocha */
 
+import { createPatient, loadApp, logIn, setCodeSession, showHomeScreen } from './init.detox';
+
 const detox = require('detox');
-const { waitFor } = require('@babel/core/lib/gensync-utils/async');
 const config = require('../package.json').detox;
+const adapter = require('detox/runners/jest/adapter');
 
 describe('liwi Native Detox test', () => {
-  beforeEach(async () => {
-    if (typeof device == 'undefined') {
-      await detox.init(config);
-    }
-  });
-
-  it('should create new session on tablet, login and go to home', async () => {
-    await expect(element(by.id('UnLockSession'))).toBeVisible();
-
-    await element(by.id('new_session')).tap();
-    await waitFor(element(by.id('new_session')).atIndex(1))
-      .toBeVisible()
-      .withTimeout(8000);
-
-    await element(by.id('connect_main')).tap();
-    await waitFor(element(by.id('SetCodeSession')).atIndex(1))
-      .toBeVisible()
-      .withTimeout(8000);
-
-    await element(by.id('set_code')).tap();
-
-    await waitFor(element(by.id('HomeScreen')).atIndex(1))
-      .toBeVisible()
-      .withTimeout(8000);
-
-    await element(by.id('GoToPatientUpsert')).tap();
-
-    await waitFor(element(by.id('PatientUpsertScreen')).atIndex(1))
-      .toBeVisible()
-      .withTimeout(8000);
-  });
+  loadApp();
+  logIn();
+  setCodeSession();
+  showHomeScreen();
+  createPatient();
 });
