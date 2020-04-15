9eec8301f10c761722f0ab95d00deab092277939
Mickaël Lacombe <mickael.lacombe@wavemind.ch>
2020-04-15 09:30:23 +0200

    change model and style
    Former-commit-id: cf1cd40f8a3363c5d73ab27a0c28e707d8f5a516

--- src/components/FinalDiagnostic/FinalDiagnostic.style.js
+++ src/components/FinalDiagnostic/FinalDiagnostic.style.js
@@ -2,10 +2,6 @@ import { StyleSheet } from 'react-native';
 import { liwiColors } from '../../utils/constants';
 
 export const styles = StyleSheet.create({
-  spaceText: {
-    // marginBottom: 30,
-    // marginLeft: 20,
-  },
   redIcon: {
     color: liwiColors.redColor,
   },
