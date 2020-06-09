47cd21b2d8dac4c1ecc4bedaedad8386224865b9
Quentin Girard <quentin.girard@wavemind.ch>
2020-06-09 10:07:46 +0200

    Destructuration of isReadOnly
    Former-commit-id: d09875a5f05c340ae030a102269f661557181bb9

--- src/components/QuestionsContainer/DisplaysContainer/Date/Date.js
+++ src/components/QuestionsContainer/DisplaysContainer/Date/Date.js
@@ -27,7 +27,7 @@ export default class Date extends React.Component<Props, State> {
   };
 
   render() {
-    const { question } = this.props;
+    const { question, isReadOnly } = this.props;
 
     const value = question.value === null ? null : moment(question.value).toDate();
 
@@ -43,7 +43,7 @@ export default class Date extends React.Component<Props, State> {
           textStyle={styles.textColor}
           placeHolderTextStyle={styles.placeholder}
           onDateChange={this._onEndEditing}
-          disabled={this.props.isReadOnly}
+          disabled={isReadOnly}
         />
       </View>
     );
