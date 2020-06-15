f125ba1f774c574e74354423229bbed98d71223b
Quentin Girard <quentin.girard@wavemind.ch>
2020-06-15 13:28:08 +0200

    Add BugSnag
    Former-commit-id: e46f3079c69e4a2be3c3a03af80505fce96fb575

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import 'react-native-gesture-handler';
 import 'core-js';
 import 'es6-symbol/implement';
 import { AppRegistry } from 'react-native';
+import { Client } from 'bugsnag-react-native';
+
 import App from './src/index';
 import { name as appName } from './app.json';
 import './src/utils/JSExceptionHandler';
@@ -14,5 +16,7 @@ import './src/utils/NativeExceptionHandler';
  * */
 // eslint-disable-next-line no-unused-vars
 import Prototype from './src/utils/Prototype.native';
+
+const bugsnag = new Client('ff8bb928f246270295b29d01f72bc641');
 
 AppRegistry.registerComponent(appName, () => App);
