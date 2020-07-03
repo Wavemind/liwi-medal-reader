492a6f59b932d6789d36168cd08d196ca359c04a
Quentin Girard <quentin.girard@wavemind.ch>
2020-07-03 15:30:37 +0200

    Fixe issue in stepper validation + start creating new page of synchronization
    Former-commit-id: 416960b2a5c29bfd71fed0be4cd8bad1a2a872e6

--- src/screens/synchronization/Synchronization.screen.js
+++ src/screens/synchronization/Synchronization.screen.js
@@ -2,11 +2,14 @@
 
 import * as React from 'react';
 import { NavigationScreenProps } from 'react-navigation';
-import { Text, View } from 'native-base';
+import { Button, Text, View } from 'native-base';
 import moment from 'moment';
 import { getItem, getItems } from '../../engine/api/LocalStorage';
 import LiwiLoader from '../../utils/LiwiLoader';
 import { LiwiTitle2 } from '../../template/layout';
+import { styles } from '../algorithmsContainer/Algorithms/Algorithms.style';
+import { synchronizeMedicalCases } from '../../../frontend_service/api/Http';
+import LiwiProgressBar from '../../utils/LiwiProgressBar';
 
 type Props = NavigationScreenProps & {};
 type State = {};
@@ -19,6 +22,7 @@ export default class Synchronization extends React.Component<Props, State> {
       patients: [],
       medicalCasesToSynch: [],
       isReady: false,
+      isLoading: false,
     };
   }
 
@@ -33,6 +37,8 @@ export default class Synchronization extends React.Component<Props, State> {
     // Retrieve all medical cases need to be synchronized
     patients.map((patient) =>
       patient.medicalCases.forEach((medicalCase) => {
+        // TODO: test if has to be push (eligibility, etc.)
+        console.log(medicalCase);
         if (medicalCase.synchronized_at === null || moment(medicalCase.updated_at) > moment(medicalCase.synchronized_at)) {
           if (reduxMedicalCase.id === medicalCase.id) {
             medicalCasesToSynch.push(reduxMedicalCase);
@@ -51,16 +57,38 @@ export default class Synchronization extends React.Component<Props, State> {
     });
   }
 
+  synchronize = async () => {
+    const { medicalCasesToSynch } = this.state;
+    this.setState({ isLoading: true });
+    await synchronizeMedicalCases(medicalCasesToSynch);
+    this.setState({ isLoading: false });
+  };
+
   render() {
-    const { isReady } = this.state;
+    const {
+      app: { t },
+    } = this.props;
+    const { isReady, isLoading, medicalCasesToSynch } = this.state;
 
     return (
       <View padding-auto margin-top>
         {isReady ? (
           <>
             <LiwiTitle2 noBorder>Medical cases to sync</LiwiTitle2>
+            <Text center>{medicalCasesToSynch.length}</Text>
+            <View flex-center-row>
+              {isLoading ? (
+                <LiwiProgressBar />
+              ) : (
+                <Button style={styles.marginTop} onPress={this.synchronize} disabled={medicalCasesToSynch.length === 0}>
+                  <Text>{t('algorithms:synchronize')}</Text>
+                </Button>
+              )}
+            </View>
           </>
-        ) : <LiwiLoader />}
+        ) : (
+          <LiwiLoader />
+        )}
       </View>
     );
   }
