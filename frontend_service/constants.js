0a636de8b8e34b9c89adc295901658c387fa3619
Quentin Girard <quentin.girard@wavemind.ch>
2020-07-09 11:58:46 +0200

    Fix final diagnostic card + add new test server
    Former-commit-id: 431a416e9318297167c3389c5b3e3b8c82019bb3

--- frontend_service/constants.js
+++ frontend_service/constants.js
@@ -2,6 +2,7 @@
 export const host = 'https://liwi.wavelab.top/api/v1/';
 // export const host = 'http://192.168.31.214:3000/api/v1/';
 // export const host = 'https://medalc.unisante.ch/api/v1/';
+// export const host = 'https://liwi-test.herokuapp.com/api/v1/';
 export const hostDataServer = 'https://liwi-main-data.herokuapp.com/api/';
 
 export const secondStatusLocalData = 5000;
