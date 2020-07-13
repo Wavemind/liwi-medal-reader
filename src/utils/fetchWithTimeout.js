82d15e70875fff2291d0d3513e3551d48433195f
Alain Fresco <alain.fresco@gmail.com>
2020-07-13 10:58:27 +0200

    :poop: Typo
    Former-commit-id: e0fd3fa8d929bc78d9495a2d7070e0bff99a19a3

--- src/utils/fetchWithTimeout.js
+++ src/utils/fetchWithTimeout.js
@@ -2,7 +2,7 @@ export default function(url, options, timeout = 30000) {
   return Promise.race([
     fetch(url, options),
     new Promise((_, reject) =>
-      setTimeout(() => reject('Server are not responding'), timeout),
+      setTimeout(() => reject('Server is not responding'), timeout),
     ),
   ]);
 }
