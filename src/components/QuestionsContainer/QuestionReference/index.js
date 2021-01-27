04671495228e015ad122f0e0555d4f243386894d
Alain Fresco <alain.fresco@gmail.com>
2021-01-27 13:37:06 +0100

    :fire: remove unused component

--- src/components/QuestionsContainer/QuestionReference/index.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import QuestionsReference from './QuestionsReference';
-
-const mapStateToProps = (medicalCase) => {
-  return { medicalCase };
-};
-
-export default connect(mapStateToProps)(QuestionsReference);
