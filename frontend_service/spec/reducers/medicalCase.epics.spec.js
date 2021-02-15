9af9fe0cf525c3f1bc937685cad09f102756d92a
Quentin Girard <quentin.girard@wavemind.ch>
2021-02-15 10:46:22 +0100

    First test

--- frontend_service/spec/reducers/medicalCase.epics.spec.js
+++ frontend_service/spec/reducers/medicalCase.epics.spec.js
@@ -10,90 +10,87 @@ import {
   drugRetained,
   validMedicine,
   algorithm,
-  getDrugDoses, conditionValue,
+  getDrugDoses, conditionValue, managementRetained,
 } from '../utils';
 import { store } from '../../store';
 import { healthCaresGetQuestions } from '../../helpers/HealthCares.model';
 
-console.log = () => {
-};
+// console.log = () => {
+// };
 console.error = () => {
 };
 console.warn = () => {
 };
 
-const enableMalaria = () => {
-  setBirthDate(1, 120);
-  jestSetAnswer(214, 393); // Gender -> Female:393 Male:394
-
-  // STAGE:TRIAGE,
-  // STEP:COMPLAINT CATEGORIES
-  jestSetAnswer(461, 725); //	ALWAYS YES General	"Yes:725  No:726"
-
-  jestSetAnswer(25, 36); // History of fever
-  jestSetAnswer(3184, 2341); // Vomiting everything
-  jestSetAnswer(456, 715); // Oral rehydration challenge
-  jestSetAnswer(93, 160); // Malaria to positive
-};
-
 describe('actions', () => {
-  it('It should return: 1yr old female, with fever, cough, difficulty breathing, chest indrawing and respiratory rate 65/min = >97%ile. All drugs are available', () => {
-    setBirthDate(1, 365); //This is the child's age (in days)
-    jestSetAnswer(214, 393); // This is the child's gender (394 = Male, 393 =  Female)
-    jestSetAnswer(3, 13); //This is the child's weight (in Kg)
-    jestSetAnswer(50, 38.8); //This is the the child's Axiliary Temperature (in Celsius)
-    jestSetAnswer(97, 13); //This is the child's MUAC (in cm)
-    jestSetAnswer(461, 725);  /* General
-726 | CC21_3 - No
-725 | CC21_1 - Yes */
-    jestSetAnswer(13, 22);  /* Respiratory Complaint (Cough or Difficult Breathing)
-22 | CC13_22 - Yes
-23 | CC13_23 - No */
-    jestSetAnswer(39, 74);  /* Cough
-74 | S39_74 - Yes
-75 | S39_75 - No */
-    jestSetAnswer(40, 76);  /* Difficulty breathing
-76 | S40_76 - Yes
-77 | S40_77 - No */
-    // jestSetAnswer(2974, 2254);  /* Grunting
-// 2255 | PE133_3 - Absent
-// 2254 | PE133_1 - Present */
-    // jestSetAnswer(4048, 3063);  /* Severe difficult breathing needing referral
-// 3063 | PE217_3 - Absent
-// 3062 | PE217_1 - Present */
-    jestSetAnswer(18, 26);  /* Chest indrawing
-// 26 | PE18_26 - Yes
-// 27 | PE18_27 - No */
-    // jestSetAnswer(63, 123);  /* Stridor
-// 123 | OS2_3 - No
-// 122 | OS2_1 - Yes */
-    // jestSetAnswer(2252, 1168);  /* Are Ampicillin AND Gentamicin
-// 1169 | TQ46_3 - No
-// 1168 | TQ46_1 - Yes */
-    // jestSetAnswer(3353, 2621);  /* Wheezing
-// 2621 | TQ72_3 - Absent
-// 2620 | TQ72_1 - Present */
-//     jestSetAnswer(3547, 95);  /* Blood oxygen saturation (%)
-// 3153 | PE214_6 - Not feasible
-// 2942 | PE214_5 - <90%
-// 2941 | PE214_4 - 90-92%
-// 2940 | PE214_3 - 92-94%
-// 2939 | PE214_2 - 94-95%
-// 2938 | PE214_1 - >/=95% */
-    jestSetAnswer(5, 65);  /* Respiratory rate
-// 3155 | PE212_0 - Not feasible
-// 2931 | PE212_6 - >/=60
-// 2930 | PE212_5 - 50-60
-// 2929 | PE212_4 - 40-50
-// 2928 | PE212_3 - 30-40
-// 2927 | PE212_2 - 20-30
-// 2926 | PE212_1 - <20 */
-    // expect(drugRetained(3360)).toEqual(false);
-    // expect(drugRetained(1709)).toEqual(true);
-    // expect(drugRetained(1660)).toEqual(true);
+  it('Complicated abscess', () => {
 
-    expect(finalDiagnosticRetained(60)).toEqual(true);
-    // expect(managementRetained(1808)).toEqual(true);
-    // expect(managementRetained(3354)).toEqual(true);
+    setBirthDate(1, 1095); //This is the child's age (in days)
+
+    jestSetAnswer(214,394); // This is the child's gender (394 = Male, 393 =  Female)
+
+    jestSetAnswer(3,15); //This is the child's weight (in Kg)
+    jestSetAnswer(50,37.2); //This is the the child's Axiliary Temperature (in Celsius)
+    jestSetAnswer(97,15); //This is the child's MUAC (in cm)
+
+    jestSetAnswer(10,16);  /* Skin and hair complaint (Rashes, skin lesions, lice, measles, etc.)
+16 | CC10_16 - Yes
+17 | CC10_17 - No */
+    jestSetAnswer(25,36);  /* Fever within the last 2 days
+36 | S25_36 - Yes
+37 | S25_37 - No */
+    jestSetAnswer(3187,2345);  /* Localized skin rash / lesion
+2345 | PE157_1 - Present
+2346 | PE157_3 - Absent */
+    jestSetAnswer(358,599);  /* Abscess of skin
+599 | PE80_1 - Present
+600 | PE80_3 - Absent */
+    jestSetAnswer(364,609);  /* Abscess located on face
+608 | PE84_1 - Yes
+609 | PE84_3 - No */
+    jestSetAnswer(361,607);  /* Large surrounding cellulitis around abscess
+606 | PE83_1 - Present
+607 | PE83_3 - Absent */
+    jestSetAnswer(3349,2614);  /* Is Ampiclox (Ampicillin + cloxacillin) po available?
+2614 | TQ71_1 - Yes
+2615 | TQ71_3 - No */
+    jestSetAnswer(2052,873);  /* Drainage of abscess possible at this health facility?
+873 | TQ18_1 - Yes
+874 | TQ18_3 - No */
+
+    jestSetAnswer(359,8);  /* Abscess of skin
+599 | PE80_1 - Present
+600 | PE80_3 - Absent */
+
+    expect(conditionValue(3187,61)).toEqual(true);  /* Localized skin rash / lesion */
+
+    expect(conditionValue(358,61)).toEqual(true);  /* Abscess of skin */
+
+    expect(conditionValue(364,61)).toEqual(true);  /* Abscess located on face */
+
+    expect(conditionValue(361,61)).toEqual(true);  /* Large surrounding cellulitis around abscess */
+
+    expect(conditionValue(38,61)).toEqual(true);  /* Fever */
+
+    expect(finalDiagnosticRetained(363)).toEqual(true);
+
+    validFinalDiagnostic(363, 'additional');
+
+
+    // TODO: MUST BE SHOWN
+    expect(conditionValue(3349,61)).toEqual(true);  /* Is Ampiclox (Ampicillin + cloxacillin) po available? */
+
+    expect(conditionValue(2052,61)).toEqual(true);  /* Drainage of abscess possible at this health facility? */
+
+    expect(drugRetained(3258)).toEqual(true);
+
+    expect(drugRetained(1660)).toEqual(true);
+
+
+    // TODO: FIXE management exclusion
+    // expect(managementRetained(2051)).toEqual(true);
+    // expect(managementRetained(1769)).toEqual(true);
+    // expect(managementRetained(1812)).toEqual(true);
+
   });
 });
--- frontend_service/store.js
+++ frontend_service/store.js
@@ -3,16 +3,16 @@ import { createEpicMiddleware } from 'redux-observable';
 import thunk from 'redux-thunk';
 import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
 import { persistReducer, persistStore } from 'redux-persist';
-import FilesystemStorage from 'redux-persist-filesystem-storage';
-// import AsyncStorage from '@react-native-community/async-storage'; // FOR TESTING
+// import FilesystemStorage from 'redux-persist-filesystem-storage';
+import AsyncStorage from '@react-native-community/async-storage'; // FOR TESTING
 import rootReducer from './reducers';
 import rootEpic from './algorithm/epics.algo';
 
 const persistConfig = {
   debug: __DEV__,
   key: 'medicalCase',
-  storage: FilesystemStorage,
-  // storage: AsyncStorage, // FOR TESTING
+  // storage: FilesystemStorage,
+  storage: AsyncStorage, // FOR TESTING
   timeout: 10000,
   stateReconciler: hardSet, // see "Merge Process" section for details.
 };
