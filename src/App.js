fc6ddd59d7f3543002932d73d0f628553bd01a0f
Quentin Girard <quentin.girard@wavemind.ch>
2021-05-04 18:00:45 +0200

    Fallback if there is no new algorithm + store in file system emergency content

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import 'react-native-gesture-handler'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/lib/integration/react'
+import FlashMessage from 'react-native-flash-message'
+
 import { store, persistor } from '@/Store'
 import { ApplicationNavigator } from '@/Navigators'
-import FlashMessage from 'react-native-flash-message'
+
 import './Translations'
 
 const App = () => (
--- src/Services/Algorithm/FetchOne.js
+++ src/Services/Algorithm/FetchOne.js
@@ -1,5 +1,8 @@
-import api from '@/Services'
 import { getMacAddress } from 'react-native-device-info'
+import { DocumentDirectoryPath, writeFile } from 'react-native-fs'
+
+import api from '@/Services'
+import { store } from '@/Store'
 
 export default async ({ json_version }) => {
   const macAddress = await getMacAddress()
@@ -10,5 +13,33 @@ export default async ({ json_version }) => {
     mac_address: macAddress,
   })
 
-  return response.data
+  // If algorithm doesn't change. Load current stored.
+  if (response.status === 204) {
+    const state = store.getState()
+    const oldAlgorithm = state.algorithm.item
+
+    return oldAlgorithm
+  }
+
+  // Store emergency content in file
+  const targetPath = `${DocumentDirectoryPath}/emergency_content.html`
+  await writeFile(targetPath, response.data.emergency_content)
+
+  // Regroup nodes, final diagnostics and health cares into nodes key
+  const nodes = {
+    ...response.data.nodes,
+    ...response.data.final_diagnostics,
+    ...response.data.health_cares,
+  }
+
+  // Remove unless key
+  delete response.data.emergency_content
+  delete response.data.nodes
+  delete response.data.final_diagnostics
+  delete response.data.health_cares
+
+  return {
+    ...response.data,
+    nodes: { ...nodes },
+  }
 }
