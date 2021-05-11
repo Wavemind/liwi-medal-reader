c07f714adb9603c3295987c82182dbed669b36e3
Quentin Girard <quentin.girard@wavemind.ch>
2021-05-11 15:14:51 +0200

    Add drawer

--- src/Navigators/Main.js
+++ src/Navigators/Main.js
@@ -3,7 +3,6 @@
  */
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
-import { createStackNavigator } from '@react-navigation/stack'
 import { useTranslation } from 'react-i18next'
 
 /**
@@ -12,38 +11,27 @@ import { useTranslation } from 'react-i18next'
 import { IndexExampleContainer, IndexSettingsContainer } from '@/Containers'
 
 const Drawer = createDrawerNavigator()
-const Stack = createStackNavigator()
 
 const MainNavigator = () => {
   const { t } = useTranslation()
 
   return (
-    <Drawer.Navigator
-      drawerContentOptions={{
-        activeTintColor: '#e91e63',
-      }}
-      initialRouteName="Home"
-    >
-      <Drawer.Screen name="Home">
-        {() => (
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Home"
-              component={IndexExampleContainer}
-              options={{ title: t('navigation.home') }}
-            />
-            <Stack.Screen
-              name="Settings"
-              component={IndexSettingsContainer}
-              options={{ title: t('navigation.settings') }}
-            />
-          </Stack.Navigator>
-        )}
-      </Drawer.Screen>
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen
+        name="Home"
+        component={IndexExampleContainer}
+        options={{
+          title: t('navigation.home'),
+          headerShown: true,
+        }}
+      />
       <Drawer.Screen
         name="Settings"
         component={IndexSettingsContainer}
-        options={{ title: t('navigation.settings') }}
+        options={{
+          title: t('navigation.settings'),
+          headerShown: true,
+        }}
       />
     </Drawer.Navigator>
   )
