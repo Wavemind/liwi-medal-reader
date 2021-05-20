0165bab80ce11b98362da6a4ccb65570174704ee
Alain Fresco <alain.fresco@gmail.com>
2021-05-20 18:18:59 +0200

    :poop: Reset Home page as first page

--- src/Navigators/Main.js
+++ src/Navigators/Main.js
@@ -26,7 +26,7 @@ const MainNavigator = () => {
   return (
     <>
       <Drawer.Navigator
-        initialRouteName="StageWrapper"
+        initialRouteName="Home"
         drawerContent={props => <CustomDrawerContent {...props} />}
         screenOptions={{
           headerShown: true,
@@ -44,7 +44,6 @@ const MainNavigator = () => {
             }),
           }}
         />
-
         <Drawer.Screen
           name="StageWrapper"
           component={StageWrapper}
