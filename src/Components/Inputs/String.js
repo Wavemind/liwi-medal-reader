f0d42746031c785b7d88cf34fdaf01d42519789f
Alain Fresco <alain.fresco@gmail.com>
2021-06-04 13:56:05 +0200

    :construction: SetAnswer in all kind of nodes

--- src/Components/Inputs/String.js
+++ src/Components/Inputs/String.js
@@ -1,16 +1,19 @@
 /**
  * The external imports
  */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { TextInput } from 'react-native'
+import { useDispatch } from 'react-redux'
 
 /**
  * The internal imports
  */
 import { useTheme } from '@/Theme'
+import SetAnswer from '@/Store/MedicalCase/SetAnswer'
 
 const String = ({ question, editable = true }) => {
   // Theme and style elements deconstruction
+  const dispatch = useDispatch()
 
   const {
     Components: { string },
@@ -28,6 +31,15 @@ const String = ({ question, editable = true }) => {
     const value = e.nativeEvent.text
     console.log('onEndEditing', value)
   }
+
+  useEffect(() => {
+    const updateAnswer = async () => {
+      if (question.value !== value) {
+        await dispatch(SetAnswer.action({ nodeId: question.id, value }))
+      }
+    }
+    updateAnswer()
+  }, [value])
 
   return (
     <TextInput
