9c6c52cdf86e6d4489f7333f62280a3d144cb56a
Quentin Girard <quentin.girard@wavemind.ch>
2021-06-07 19:04:49 +0200

    Add test for validation

--- __tests__/Services/MedicalCase/Validation.test.js
+++ __tests__/Services/MedicalCase/Validation.test.js
@@ -6,26 +6,40 @@ import 'react-native'
 /**
  * The internal imports
  */
-import validationMedicalCaseService from '@/Services/MedicalCase/Validation'
-import createMedicalCaseService from '@/Services/MedicalCase/Create'
-import { translate } from '@/Translations/algorithm'
-import LoadAlgorithm from '@/Store/Algorithm/Load'
-import { store } from '@/Store'
+ import validationMedicalCaseService from '@/Services/MedicalCase/Validation'
+ import createMedicalCase from '@/Store/MedicalCase/Create'
+ import { translate } from '@/Translations/algorithm'
+ import LoadAlgorithm from '@/Store/Algorithm/Load'
+ import { store } from '@/Store'
 
 beforeAll(async () => {
   const algorithmFile = require('../../algorithm.json')
-
   await store.dispatch(
     LoadAlgorithm.action({
       newAlgorithm: algorithmFile,
     }),
   )
-
   const algorithm = store.getState().algorithm.item
-  await createMedicalCaseService({ algorithm })
+  await store.dispatch(createMedicalCase.action({ algorithm }))
 })
 
-describe('Handle error message', () => {
+describe('Handle validations message', () => {
+  it('should not have error', async () => {
+    const algorithm = store.getState().algorithm.item
+    const medicalCase = store.getState().medicalCase.item
+
+    // Axillary temperature (in XX.X °C)
+    const node = algorithm.nodes[50]
+    const mcNode = medicalCase.nodes[50]
+
+    // Value to 37.7°
+    const result = await validationMedicalCaseService(mcNode, node, '37.7')
+    expect(result).toStrictEqual({
+      validationMessage: null,
+      validationType: null,
+    })
+  })
+
   it('should handle error message without warning message filled in node', async () => {
     const algorithm = store.getState().algorithm.item
     const medicalCase = store.getState().medicalCase.item
@@ -34,10 +48,32 @@ describe('Handle error message', () => {
     const node = algorithm.nodes[50]
     const mcNode = medicalCase.nodes[50]
 
-    const result = await validationMedicalCaseService(mcNode, node, 63)
+    // Value to 63°
+    const result = await validationMedicalCaseService(mcNode, node, '63')
     expect(result).toStrictEqual({
       validationMessage: translate(node.max_message_error),
       validationType: 'error',
+    })
+  })
+
+  it('should avoid validation if an unavailableValue is set', async () => {
+    const algorithm = store.getState().algorithm.item
+    const medicalCase = store.getState().medicalCase.item
+
+    // Axillary temperature (in XX.X °C)
+    const node = algorithm.nodes[50]
+    const mcNode = medicalCase.nodes[50]
+    mcNode.unavailableValue = true
+
+    // Child feels hot = 38°
+    const result = await validationMedicalCaseService(
+      mcNode,
+      node,
+      Object.values(node.answers)[0].value,
+    )
+    expect(result).toStrictEqual({
+      validationMessage: null,
+      validationType: null,
     })
   })
 })
