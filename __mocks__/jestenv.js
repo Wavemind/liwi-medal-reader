844acd2ec968d2be7dcfd3c4ba8f191d10668944
Quentin Girard <quentin.girard@wavemind.ch>
2021-06-07 19:18:30 +0200

    Remove unused files

--- __mocks__/jestenv.js
+++ __mocks__/jestenv.js
@@ -1,7 +1,9 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
 import { NativeModules } from 'react-native'
 import mockNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js'
+import 'react-native-gesture-handler/jestSetup'
 
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
 jest.mock('@react-native-community/netinfo', () => mockNetInfo)
 jest.mock('@react-native-community/async-storage', () => mockAsyncStorage)
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
