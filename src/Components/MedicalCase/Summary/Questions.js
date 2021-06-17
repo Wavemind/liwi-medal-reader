ba0f64aea3af9c91856b87893592de31a799513a
Sinan Ucak <ucak.sinan@gmail.com>
2021-06-17 17:32:56 +0200

    removed node removal into a service

--- src/Components/MedicalCase/Summary/Questions.js
+++ src/Components/MedicalCase/Summary/Questions.js
@@ -3,47 +3,20 @@
  */
 import React from 'react'
 import { FlatList, View } from 'react-native'
-import { useSelector } from 'react-redux'
-import differenceInDays from 'date-fns/differenceInDays'
-import startOfToday from 'date-fns/startOfToday'
 
 /**
  * The internal imports
  */
 import { useTheme } from '@/Theme'
 import { QuestionItem } from '@/Components'
-import { Config } from '@/Config'
+import { removeQuestions } from '@/Services/MedicalCase/RemoveQuestions'
 
 const PersonalInfoPatientContainer = () => {
   const {
     Containers: { patientPersonalInfo },
   } = useTheme()
 
-  const nodes = useSelector(state => state.medicalCase.item.nodes)
-  const algorithm = useSelector(state => state.algorithm.item)
-  const birthDate = useSelector(state => state.patient.item.birth_date)
-
-  // Remove / Keep neonat question based on patient age
-  const questions = { ...nodes }
-  const neonatCCs = algorithm.mobile_config.questions_orders[
-    Config.STEP_ORDERS.complaintCategories
-  ].filter(ccId => algorithm.nodes[ccId].is_neonat)
-  const days = birthDate ? differenceInDays(startOfToday(), birthDate) : 0
-
-  Object.keys(nodes).forEach(nodeId => {
-    const mcNode = algorithm.nodes[nodeId]
-    // If patient is older than 60 days, we remove YI questions
-    // Or if nodeId is a QuestionsSequence, we remove the question
-    if (
-      (days > 60 &&
-        mcNode.conditioned_by_cc !== undefined &&
-        mcNode.conditioned_by_cc.length > 0 &&
-        mcNode.conditioned_by_cc.some(ccId => neonatCCs.includes(ccId))) ||
-      mcNode.type === 'QuestionsSequence'
-    ) {
-      delete questions[parseInt(nodeId, 10)]
-    }
-  })
+  const questions = removeQuestions()
 
   return (
     <View style={[patientPersonalInfo.wrapper]}>
