5cee8f74f986acf176a4c5753f218961b26831f0
Quentin Girard <quentin.girard@wavemind.ch>
2021-06-29 10:47:51 +0200

    Remove duplication of medical case when a new one is created

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,3 +1,4 @@
 export { default as usePrevious } from './UsePrevious'
 export { default as QuestionStepValidation } from './QuestionStepValidation'
 export { default as GenerateUpdatePatient } from './GenerateUpdatePatient'
+export { default as UniqObject } from './UniqObject'
