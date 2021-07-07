395b1d8ecd515d608fdbeb74175cced1b8a55d37
Alain Fresco <alain.fresco@gmail.com>
2021-07-07 17:12:21 +0200

    :sparkles: Create function to comunicate with medal-hub

---
+++ src/Services/Database/Remote/index.js
@@ -0,0 +1,101 @@
+import axios from 'axios'
+import { showMessage } from 'react-native-flash-message'
+import i18n from '@/Translations/index'
+import { store } from '@/Store'
+
+const instance = axios.create({
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  timeout: 3000,
+})
+/**
+ * Handles the error returned from the api
+ * @param message
+ * @param data
+ * @param status
+ * @returns {Promise<unknown>}
+ */
+export const handleError = ({ message, data, status }) => {
+  return Promise.reject({ message, data, status })
+}
+
+instance.interceptors.request.use(
+  async function (config) {
+    const state = store.getState()
+    const { clinician, local_data_ip } = state.healthFacility.item
+    const mac_address = state.device.item.mac_address
+
+    config.baseURL = local_data_ip
+    config.headers.common.mac_address = mac_address
+    config.headers.common.clinician = clinician
+    return config
+  },
+  function (error) {
+    console.log('error request', error)
+    // Do something with request error
+    return Promise.reject(error)
+  },
+)
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+  },
+  async function (error) {
+    if (error.response) {
+      // Default response
+      let errorMessage = 'Response status code <> 200 (' + error.message + ')'
+
+      // Response given by the application
+      if (error.response.data.errors) {
+        if (Array.isArray(error.response.data.errors)) {
+          errorMessage = error.response.data.errors[0]
+        } else {
+          errorMessage = error.response.data.errors
+        }
+      }
+
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      return handleError({
+        message: errorMessage,
+        status: error.response.status,
+        data: error.response.data,
+      })
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      showMessage({
+        message: i18n.t('errors.offline.title'),
+        description: i18n.t('errors.offline.description'),
+        type: 'danger',
+        duration: 5000,
+      })
+
+      return handleError({
+        message: 'No response received (' + error.message + ')',
+      })
+    } else {
+      // Something happened in setting up the request that triggered an Error
+
+      showMessage({
+        message: i18n.t('errors.unknown.title'),
+        description: i18n.t('errors.unknown.description'),
+        type: 'danger',
+        duration: 5000,
+      })
+
+      return handleError({
+        message: 'Unknown error (' + error.message + ')',
+      })
+    }
+  },
+)
+
+export default instance
--- src/Services/Database/Remote/useRemoteInterface.js
+++ src/Services/Database/Remote/useRemoteInterface.js
@@ -1,26 +1,37 @@
 /**
  * The external imports
  */
-import { useSelector } from 'react-redux'
 
 /**
  * The internal imports
  */
+import api from '@/Services/Database/Remote'
 
 export default function () {
-  const healthFacility = useSelector(state => state.healthFacility.item)
-  const macAddress = useSelector(state => state.device.item.mac_addresss)
-  const localDataIp = healthFacility.local_data_ip
+  /**
+   * Returns all the entry on a specific model
+   * @param { string } model - The model name of the data we want to retrieve
+   * @param { integer } page - Pagination. if null, retrieved all information
+   * @param { object } params - options for the request the search query and the filter is in there
+   * @returns { Collection } - A collection of all the data
+   */
+  const getAll = async (model, page, params) => {
+    const stringFilters = _generateFiltersUrl(params.filters)
+    const url = `/api/${_mapModelToRoute(model)}?page=${page}&query=${
+      params.query
+    }${stringFilters !== '' ? `&filter=${stringFilters}` : ''}`
+
+    const response = await api.get(url)
+    return response.data.data
+  }
 
   /**
-   * Creates an entry of a specific model in the database
-   * @param { string } model - The model name of the data we want to retrieve
-   * @param { object } object - The value of the object
+   * Get all consent file for all users
+   * @returns {Promise<string|Array>}
    */
-  const insert = async (model, object) => {
-    // const url = `${localDataIp}/api/${_mapModelToRoute(model)}`
-    // const header = await _setHeaders('POST', object)
-    // return _fetch(url, header)
+  const getConsentsFile = async page => {
+    const response = await api.post('/api/patients/consent_files', { page })
+    return response.data.data
   }
 
   /**
@@ -31,73 +42,68 @@ export default function () {
    * @returns { Collection } - The wanted object
    */
   const findBy = async (model, value, field) => {
-    // const url = `${localDataIp}/api/${_mapModelToRoute(
-    //   model,
-    // )}/find_by?field=${field}&value=${value}`
-    // const header = await _setHeaders()
-    // const data = await _fetch(url, header)
-    // if (data !== null) {
-    //   // TODO probably don't want to create Object
-    //   //return _initClasses(data, model)
-    // }
-    // return data
+    const url = `/api/${_mapModelToRoute(
+      model,
+    )}/find_by?field=${field}&value=${value}`
+
+    const response = await api.get(url)
+    return response.data.data
   }
 
   /**
-   * Returns all the entry on a specific model
-   * @param { string } model - The model name of the data we want to retrieve
-   * @param { integer } page - Pagination. if null, retrieved all information
-   * @param { object } params - options for the request the search query and the filter is in there
-   * @returns { Collection } - A collection of all the data
+   * Create activities for a related medical case
+   * @param { integer } medicalCaseId
+   * @param { array } activities - List of activities to create
+   * @private
    */
-  const getAll = async (model, page, params) => {
-    // const stringFilters = _generateFiltersUrl(params.filters)
-    // const url = `${localDataIp}/api/${_mapModelToRoute(
-    //   model,
-    // )}?page=${page}&query=${params.query}${
-    //   stringFilters !== '' ? `&filter=${stringFilters}` : ''
-    // }`
-    // const header = await _setHeaders()
-    // return _fetch(url, header)
+  const insertActivities = async (medicalCaseId, activities) => {
+    const response = await api.post(
+      `/api/medical_cases/${medicalCaseId}/activities`,
+      activities,
+    )
+    return response.data.data
+  }
+  
+  /**
+   * Inserts a new medical case into the DB
+   * @param patientId
+   * @param medicalCaseData
+   * @returns {Promise<void>}
+   */
+  const insertMedicalCase = async (patientId, medicalCaseData) => {
+    const response = await api.post(
+      `/api/patients/${patientId}/medical_cases`,
+      medicalCaseData,
+    )
+    return response.data.data
   }
 
   /**
-   * Update or insert value in a existing row
-   * @param { string } model - The model name of the data we want to retrieve
-   * @param { integer } id - The row to update
-   * @param { string } fields - The field to update
-   * @param { boolean } updatePatientValue - Used only in RealmInterface
-   * @returns { Collection } - Updated object
+   * Inserts the patient values to the DB.
+   * @param patientValues
+   * @param patientId
+   * @returns {Promise<void>}
    */
-  const update = async (model, id, fields, updatePatientValue) => {
-    // const url = `${localDataIp}/api/${_mapModelToRoute(model)}/${id}`
-    // const header = await _setHeaders('PUT', fields)
-    // return _fetch(url, header)
+  const insertPatient = async (patientData, medicalCaseData) => {
+    const response = await api.post('/api/patients', {
+      medical_case: medicalCaseData,
+      patient: patientData,
+    })
+    return response.data.data
   }
 
   /**
-   * Push an object in a existing object based on model name and id
-   * @param { string } model - The model name of the data we want to retrieve
-   * @param { integer } id - The row to update
-   * @param { string } field - The field to update
-   * @param { any } value - value to update
-   * @returns { Collection } - Updated object
+   * Inserts the patient values to the DB.
+   * @param patientValues
+   * @param patientId
+   * @returns {Promise<void>}
    */
-  const push = async (model, id, field, value) => {
-    // const url = `${localDataIp}/api/${_mapModelToRoute(model)}/${id}`
-    // const header = await _setHeaders('PUT', { [field]: value })
-    // return _fetch(url, header)
-  }
-
-  /**
-   * Unlock a medical case when device is in client server architecture
-   * @param {integer} id - Medical case id
-   * @returns {string}
-   */
-  const unlockMedicalCase = async id => {
-    // const url = `${localDataIp}/api/medical_cases/${id}/unlock`
-    // const header = await _setHeaders()
-    // return _fetch(url, header)
+  const insertPatientValues = async (patientValues, patientId) => {
+    const response = await api.post(
+      `/api/patients/${patientId}/patient_values`,
+      patientValues,
+    )
+    return response.data.data
   }
 
   /**
@@ -106,9 +112,33 @@ export default function () {
    * @returns {string}
    */
   const lockMedicalCase = async id => {
-    // const url = `${localDataIp}/api/medical_cases/${id}/lock`
-    // const header = await _setHeaders()
-    // return _fetch(url, header)
+    const response = await api.post(`/api/medical_cases/${id}/lock`)
+    return response.data.data
+  }
+
+  /**
+   * Unlock a medical case when device is in client server architecture
+   * @param {integer} id - Medical case id
+   * @returns {string}
+   */
+  const unlockMedicalCase = async id => {
+    const response = await api.post(`/api/medical_cases/${id}/unlock`)
+    return response.data.data
+  }
+
+  /**
+   * Update or insert value in a existing row
+   * @param { string } model - The model name of the data we want to retrieve
+   * @param { integer } id - The row to update
+   * @param { string } fields - The field to update
+   * @returns { Collection } - Updated object
+   */
+  const update = async (model, id, fields) => {
+    const response = await api.patch(
+      `/api/${_mapModelToRoute(model)}/${id}`,
+      fields,
+    )
+    return response.data.data
   }
 
   /**
@@ -117,43 +147,8 @@ export default function () {
    * @returns {Promise<string|Array>}
    */
   const synchronizePatients = async patients => {
-    // const url = `${localDataIp}/api/patients/synchronize`
-    // const header = await _setHeaders('POST', { patients })
-    // return _fetch(url, header)
-  }
-
-  /**
-   * Get all consent file for all users
-   * @returns {Promise<string|Array>}
-   */
-  const getConsentsFile = async page => {
-    // const url = `${localDataIp}/api/patients/consents_file?page=${page}`
-    // const header = await _setHeaders()
-    // return _fetch(url, header)
-  }
-
-  /**
-   * Make the request and parse result
-   * @param { string } url - Url to bind
-   * @param { object } header - Header options
-   * @returns {Promise<string|array>}
-   * @private
-   */
-  const _fetch = async (url, header) => {
-    // const httpRequest = await fetch(url, header).catch(error => {
-    //   handleHttpError(error)
-    // })
-    // // In case of fetch timeout
-    // if (httpRequest !== undefined) {
-    //   const result = await httpRequest.json()
-    //   if (httpRequest.status === 200) {
-    //     return result
-    //   }
-    //   if (httpRequest.status > 404) {
-    //     handleHttpError(result.message)
-    //   }
-    // }
-    // return null
+    const response = await api.post('/api/patients/synchronize', patients)
+    return response.data.data
   }
 
   /**
@@ -163,55 +158,18 @@ export default function () {
    * @private
    */
   const _mapModelToRoute = model => {
-    // let route = ''
-    // switch (model) {
-    //   case 'Patient':
-    //     route = 'patients'
-    //     break
-    //   case 'MedicalCase':
-    //     route = 'medical_cases'
-    //     break
-    //   default:
-    //     console.warn("route doesn't exist", model)
-    // }
-    // return route
-  }
-
-  /**
-   * Keeps the undefined values when Json.stringify
-   */
-  const _replacer = (key, value) =>
-    typeof value === 'undefined' ? null : value
-
-  /**
-   * Set header credentials to communicate with server
-   * @params [String] method
-   * @params [Object] body
-   * @return [Object] header
-   * @private
-   */
-  const _setHeaders = async (method = 'GET', body = false) => {
-    // if (clinician === null) {
-    //   await _setClinician()
-    // }
-    // const header = {
-    //   method,
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //     'x-mac-address': macAddress,
-    //     'x-clinician': clinician,
-    //   },
-    // }
-    // if (
-    //   method === 'POST' ||
-    //   method === 'PATCH' ||
-    //   method === 'PUT' ||
-    //   method === 'DELETE'
-    // ) {
-    //   header.body = JSON.stringify(body, _replacer)
-    // }
-    // return header
+    let route = ''
+    switch (model) {
+      case 'Patient':
+        route = 'patients'
+        break
+      case 'MedicalCase':
+        route = 'medical_cases'
+        break
+      default:
+        console.warn("route doesn't exist", model)
+    }
+    return route
   }
 
   /**
@@ -221,64 +179,34 @@ export default function () {
    * @private
    */
   const _generateFiltersUrl = filters => {
-    // let stringFilters = ''
-    // if (filters.length !== 0) {
-    //   Object.keys(filters).forEach((nodeId, key) => {
-    //     stringFilters += `${nodeId}:`
-    //     filters[nodeId].map((filter, filterKey) => {
-    //       stringFilters += filter
-    //       if (filterKey + 1 < filters[nodeId].length) {
-    //         stringFilters += ','
-    //       }
-    //     })
-    //     if (key + 1 < Object.keys(filters).length) {
-    //       stringFilters += '|'
-    //     }
-    //   })
-    // }
-    // return stringFilters
+    let stringFilters = ''
+    if (filters.length !== 0) {
+      Object.keys(filters).forEach((nodeId, key) => {
+        stringFilters += `${nodeId}:`
+        filters[nodeId].map((filter, filterKey) => {
+          stringFilters += filter
+          if (filterKey + 1 < filters[nodeId].length) {
+            stringFilters += ','
+          }
+        })
+        if (key + 1 < Object.keys(filters).length) {
+          stringFilters += '|'
+        }
+      })
+    }
+    return stringFilters
   }
 
-  /**
-   * Generate class
-   * @param { array|object } data - Data retrieved from server
-   * @param { string } model - Class name
-   * @returns {Promise<[]|PatientModel|MedicalCaseModel>}
-   * @private
-   */
-
-  // TODO Probably don't want to create objects
-  //   const _initClasses = async (data, model) => {
-  //     const object = []
-  //     const environment = await getEnvironment()
-
-  //     if (model === 'Patient') {
-  //       if (data instanceof Array) {
-  //         data.forEach(async item => {
-  //           const newPatient = await new PatientModel(item, environment)
-  //           object.push(newPatient)
-  //         })
-  //       } else {
-  //         return new PatientModel(data, environment)
-  //       }
-  //     } else if (data instanceof Array) {
-  //       data.forEach(item => {
-  //         object.push(new MedicalCaseModel(item))
-  //       })
-  //     } else {
-  //       return new MedicalCaseModel(data)
-  //     }
-  //     return object
-  //   }
-
   return {
-    insert,
     findBy,
+    insertActivities,
+    insertPatient,
+    insertPatientValues,
     getAll,
     update,
-    push,
-    unlockMedicalCase,
+    insertMedicalCase,
     lockMedicalCase,
+    unlockMedicalCase,
     synchronizePatients,
     getConsentsFile,
   }
