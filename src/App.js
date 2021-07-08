724e607ad915dbaf61d868a20d179464c6421f47
Quentin Girard <quentin.girard@wavemind.ch>
2021-07-08 16:28:49 +0200

    Avoid calculation of reference table if questions needed isn't answered

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { View, Text } from 'react-native'
+import { ScrollView, Text } from 'react-native'
 import { PersistGate } from 'redux-persist/lib/integration/react'
 import FlashMessage from 'react-native-flash-message'
 import Appsignal from '@appsignal/javascript'
@@ -9,12 +9,14 @@ import { ErrorBoundary } from '@appsignal/react'
 
 const appsignal = new Appsignal({ key: '9c0f7538-551f-41a5-b331-864ba2e04705' })
 const FallbackComponent = error => (
-  <View style={{ flex: 1 }}>
-    <Text style={{ fontSize: 20, color: 'white' }}>AN ERROR OCCURED</Text>
+  <ScrollView style={{ flex: 1 }}>
+    <Text style={{ fontSize: 20, color: 'white', marginBottom: 50 }}>
+      AN ERROR OCCURED
+    </Text>
     <Text style={{ fontSize: 18, color: 'white' }}>
       {JSON.stringify(error)}
     </Text>
-  </View>
+  </ScrollView>
 )
 
 import { store, persistor } from '@/Store'
@@ -26,7 +28,7 @@ const App = () => {
   return (
     <ErrorBoundary
       instance={appsignal}
-      fallback={error => <FallbackComponent />}
+      fallback={error => <FallbackComponent error={error} />}
     >
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
