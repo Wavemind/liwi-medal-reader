06bf2ce7f04e32ba9a6ca5ad56b93fb74175b11e
Sinan Ucak <ucak.sinan@gmail.com>
2021-07-29 13:15:56 +0200

    Corrected healthcare questions for additional diagnoses

--- src/Services/Steps/TreatmentConditions.js
+++ src/Services/Steps/TreatmentConditions.js
@@ -8,10 +8,13 @@ import { handleChildren, getTopConditions, uniq } from '@/Utils/MedicalCase'
 
 export default () => {
   const state = store.getState()
-  const finalDiagnostics = state.medicalCase.item.diagnosis.agreed
+  const agreed = state.medicalCase.item.diagnosis.agreed
+  const additional = state.medicalCase.item.diagnosis.additional
   const nodes = state.algorithm.item.nodes
   const diagnoses = state.algorithm.item.diagnoses
   const questionsToDisplay = []
+
+  const finalDiagnostics = { ...agreed, ...additional }
 
   Object.values(finalDiagnostics).forEach(agreedFinalDiagnostic => {
     const finalDiagnostic = nodes[agreedFinalDiagnostic.id]
