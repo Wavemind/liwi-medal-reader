b3cbd2da45bc6cf01cd0708e938dbf2c3f1b9654
Quentin Girard <quentin.girard@wavemind.ch>
2021-08-19 12:39:43 +0200

    Create own modal with zoom options for pictures

--- src/Components/Medias/Picture.js
+++ src/Components/Medias/Picture.js
@@ -1,32 +1,71 @@
 /**
  * The external imports
  */
-import React from 'react'
-import { Image } from 'react-native'
-import Lightbox from 'react-native-lightbox-v2'
+import React, { useState } from 'react'
+import { Image, View, TouchableOpacity } from 'react-native'
+import ImageZoom from 'react-native-image-pan-zoom'
+import Modal from 'react-native-modal'
 
 /**
  * The internal imports
  */
 import { useTheme } from '@/Theme'
+import { hp, wp } from '@/Theme/Responsive'
+import { Icon } from '@/Components'
 
 const Picture = ({ url }) => {
   // Theme and style elements deconstruction
   const {
     Components: { picture },
     Gutters,
+    Layout,
+    FontSize,
+    Colors,
   } = useTheme()
 
+  const [modalVisible, setModalVisible] = useState(false)
+
   return (
-    <Lightbox style={Gutters.regularTMargin}>
-      <Image
-        style={picture.wrapper}
-        resizeMode="center"
-        source={{
-          uri: url,
-        }}
-      />
-    </Lightbox>
+    <View style={Gutters.regularTMargin}>
+      <Modal
+        isVisible={modalVisible}
+        onBackdropPress={() => setModalVisible(false)}
+        onSwipeComplete={() => setModalVisible(false)}
+        swipeDirection="down"
+        style={picture.modal}
+      >
+        <View style={[Layout.fill]}>
+          <TouchableOpacity
+            style={[Gutters.regularHPadding, Gutters.regularVPadding]}
+            onPress={() => setModalVisible(false)}
+          >
+            <Icon name="close" size={FontSize.huge} color={Colors.white} />
+          </TouchableOpacity>
+          <ImageZoom
+            cropWidth={wp(100)}
+            cropHeight={hp(100)}
+            imageWidth={wp(100)}
+            imageHeight={hp(50)}
+          >
+            <Image
+              style={picture.wrapper}
+              source={{
+                uri: url,
+              }}
+            />
+          </ImageZoom>
+        </View>
+      </Modal>
+
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <Image
+          style={picture.wrapper}
+          source={{
+            uri: url,
+          }}
+        />
+      </TouchableOpacity>
+    </View>
   )
 }
 
--- src/Theme/components/Picture.style.js
+++ src/Theme/components/Picture.style.js
@@ -6,5 +6,8 @@ export default function (props) {
     wrapper: {
       height: hp(40),
     },
+    modal: {
+      margin: 0,
+    },
   })
 }
