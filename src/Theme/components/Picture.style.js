b16777718701d0b70cae19246694a5f242a28979
Quentin Girard <quentin.girard@wavemind.ch>
2021-08-25 13:21:49 +0200

    Resize image to be full screen

--- src/Theme/components/Picture.style.js
+++ src/Theme/components/Picture.style.js
@@ -1,10 +1,14 @@
 import { StyleSheet } from 'react-native'
-import { hp } from '@/Theme/Responsive'
+import { hp, wp } from '@/Theme/Responsive'
 
 export default function (props) {
   return StyleSheet.create({
     wrapper: {
       height: hp(40),
+    },
+    image: {
+      height: hp(100),
+      width: wp(100),
     },
     modal: {
       margin: 0,
