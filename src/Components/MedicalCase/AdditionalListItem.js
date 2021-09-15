a7b521dfec83fc527b2cfa1afa3947d91536a19e
Quentin Girard <quentin.girard@wavemind.ch>
2021-09-15 09:26:54 +0200

    Add description in drug/diagnostic additional list

--- src/Components/MedicalCase/AdditionalListItem.js
+++ src/Components/MedicalCase/AdditionalListItem.js
@@ -1,30 +1,41 @@
 /**
  * The external imports
  */
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from 'react-native'
 
 /**
  * The internal imports
  */
 import { useTheme } from '@/Theme'
-import { Checkbox } from '@/Components'
+import { Checkbox, QuestionInfoButton } from '@/Components'
 import { translate } from '@/Translations/algorithm'
 
 const AdditionalListItem = ({ item, handlePress, selected }) => {
   // Theme and style elements deconstruction
   const {
     Components: { diagnosisItem },
+    Layout,
   } = useTheme()
+
+  const [descriptionAvailable] = useState(translate(item.description) !== '')
+  const [mediaAvailable] = useState(item.medias?.length > 0)
 
   return (
     <View style={diagnosisItem.wrapper}>
-      <Checkbox
-        nodeId={item.id}
-        label={translate(item.label)}
-        onPress={() => handlePress(item)}
-        defaultValue={Object.keys(selected).includes(item.id.toString())}
-      />
+      <View style={Layout.fill}>
+        <Checkbox
+          nodeId={item.id}
+          label={translate(item.label)}
+          onPress={() => handlePress(item)}
+          defaultValue={Object.keys(selected).includes(item.id.toString())}
+        />
+      </View>
+      <View style={Layout.rowHCenter}>
+        {(descriptionAvailable || mediaAvailable || __DEV__) && (
+          <QuestionInfoButton nodeId={item.id} />
+        )}
+      </View>
     </View>
   )
 }
