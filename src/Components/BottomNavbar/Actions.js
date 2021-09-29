67aa8b1dd16e51976bd543f59ec75a91bf2f73d0
Quentin Girard <quentin.girard@wavemind.ch>
2021-09-29 15:45:15 +0200

    Improve actions component

--- src/Components/BottomNavbar/Actions.js
+++ src/Components/BottomNavbar/Actions.js
@@ -10,33 +10,24 @@ import { useSelector } from 'react-redux'
 import { SynchronizationNavbar, StageWrapperNavbar } from '@/Components'
 
 const ActionsNavbar = ({ navigationState }) => {
-  let name
-  let stageIndex
-  let stepIndex
-
   const advancement = useSelector(state => state.medicalCase.item.advancement)
+
   if (advancement !== undefined) {
-    stageIndex = advancement.stage
-    stepIndex = advancement.step
-    name = 'StageWrapper'
+    return (
+      <StageWrapperNavbar
+        stageIndex={advancement.stage}
+        stepIndex={advancement.step}
+      />
+    )
   } else {
     const homeNavigation = navigationState.routes[navigationState.index].state
     const route = homeNavigation?.routes[homeNavigation.index]
-    // Early return if navigation is not loaded
-    if (route === undefined) {
-      return null
-    }
-  }
 
-  switch (name) {
-    case 'StageWrapper':
-      return (
-        <StageWrapperNavbar stageIndex={stageIndex} stepIndex={stepIndex} />
-      )
-    case 'Synchronization':
+    if (route?.params?.name === 'Synchronization') {
       return <SynchronizationNavbar />
-    default:
-      return null
+    }
+
+    return null
   }
 }
 
