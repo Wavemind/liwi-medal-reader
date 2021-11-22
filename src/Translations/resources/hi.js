110be25ca4a73f6e638278e902c41dbef765d313
Quentin Girard <quentin.girard@wavemind.ch>
2021-11-22 16:22:05 +0100

    Update translation

--- src/Translations/resources/hi.js
+++ src/Translations/resources/hi.js
@@ -219,9 +219,9 @@ export default {
         months: 'In months',
         years: 'In years',
       },
-      title: 'Sélectionnez la date',
-      confirm: 'Confirmer',
-      cancel: 'Annuler',
+      title: 'Select date',
+      confirm: 'Confirm',
+      cancel: 'Cancel',
     },
     medical_case_drawer: {
       current_medical_case: 'Current consultation',
