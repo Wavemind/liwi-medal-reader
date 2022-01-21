c3909ec4a87b9e7ca8391365f33e11c0e78e7a1c
Quentin Girard <quentin.girard@wavemind.ch>
2022-01-21 10:43:07 +0100

    Fetch emergency content with a zip and fixe display of loader in synchronize screen

--- src/Containers/Auth/Synchronize.js
+++ src/Containers/Auth/Synchronize.js
@@ -28,6 +28,7 @@ const SynchronizeAuthContainer = () => {
   // Theme and style elements deconstruction
   const { t } = useTranslation()
   const {
+    Layout,
     Containers: { auth, authLogin, global },
   } = useTheme()
   const dispatch = useDispatch()
@@ -71,7 +72,7 @@ const SynchronizeAuthContainer = () => {
         )
         if (isFulfilled(fetchOneEmergency)) {
           setLoading(false)
-          // Navigate and reset to Pin container
+          // // Navigate and reset to Pin container
           navigateAndSimpleReset('Study', { source: 'auth' })
         }
       }
@@ -96,7 +97,9 @@ const SynchronizeAuthContainer = () => {
               onPress={fetchData}
             />
           </View>
-          <View style={authLogin.loaderWrapper}>{loading && <Loader />}</View>
+          <View style={Layout.rowCenter}>
+            <View style={authLogin.loaderWrapper}>{loading && <Loader />}</View>
+          </View>
 
           <View style={authLogin.buttonWrapper}>
             {healthFacilityFetchOneError && (
--- src/Services/Emergency/FetchOne.js
+++ src/Services/Emergency/FetchOne.js
@@ -5,35 +5,80 @@ import {
   DocumentDirectoryPath,
   writeFile,
   unlink,
+  mkdir,
   exists,
+  readFile,
 } from 'react-native-fs'
+import { unzip } from 'react-native-zip-archive'
+import ReactNativeBlobUtil from 'react-native-blob-util'
 
 /**
  * The internal imports
  */
 import api from '@/Services'
 import { store } from '@/Store'
+import { RefreshTokenAuthService } from '@/Services/Auth'
 
 export default async ({ emergencyContentVersion }) => {
+  // TODO: CLEAR WHEN ALL DATA IS UP TO DATE WItH ZIP
+  const state = store.getState()
+  let emergencyContent
+
   const response = await api.get(
     `emergency-content?json_version=${emergencyContentVersion}`,
   )
 
+  emergencyContent = response.data.emergency_content
+
   // If emergency content doesn't change. Load current stored.
-  if (response === undefined || response.status === 204) {
+  if (response.status === 204) {
     const state = store.getState()
     return state.emergency.item
+  }
+
+  if (response.headers['content-type'] === 'application/zip') {
+    // ZIP FETCH
+    const bearToken = await RefreshTokenAuthService()
+    const mainDataUrl = state.auth.medAlDataURL
+
+    const zipResponse = await ReactNativeBlobUtil.config({
+      fileCache: true,
+      appendExt: 'zip',
+    })
+      .fetch(
+        'GET',
+        `${mainDataUrl}/api/v1/emergency-content?json_version=${emergencyContentVersion}`,
+        {
+          'Content-Type': 'multipart/form-data',
+          Accept: 'application/zip',
+          Authorization: bearToken,
+        },
+      )
+      .catch(err => {
+        return Promise.reject({ message: err })
+      })
+
+    // Create tmp file to process zip
+    const targetPath = `${DocumentDirectoryPath}/tmp_emergency_content_zip`
+    await mkdir(targetPath)
+    const unzipPath = await unzip(zipResponse.path(), targetPath)
+
+    const zipContent = await readFile(unzipPath + '/content.json')
+    emergencyContent = JSON.parse(zipContent)
+
+    await unlink(unzipPath)
+    /////////////////////////////////////////////////////
   }
 
   // Store emergency content in file
   const emergencyContentTargetPath = `${DocumentDirectoryPath}/emergency_content.json`
 
-  if (response.data.emergency_content) {
+  if (emergencyContent) {
     const emergencyContentFileExist = await exists(emergencyContentTargetPath)
     if (emergencyContentFileExist) {
       await unlink(emergencyContentTargetPath)
     }
-    await writeFile(emergencyContentTargetPath, response.data.emergency_content)
+    await writeFile(emergencyContentTargetPath, emergencyContent)
   }
 
   return response.data
