492579819cb9ef6693228c7aa72b79593b0c393b
Quentin Girard <quentin.girard@wavemind.ch>
2022-02-25 09:51:26 +0100

    Fixe json and update tests

--- __tests__/Services/MedicalCase/setDiagnosesTIMCISN.test.js
+++ __tests__/Services/MedicalCase/setDiagnosesTIMCISN.test.js
@@ -10,6 +10,7 @@ import CreatePatient from '@/Store/Patient/Create'
 import CreateMedicalCase from '@/Store/MedicalCase/Create'
 import LoadAlgorithm from '@/Store/Algorithm/Load'
 import { store } from '@/Store'
+import { SetDiagnosesService } from '@/Services/MedicalCase'
 import { setBirthDate } from '../../Utils/BirthDate'
 import { setAnswer } from '../../Utils/Answer'
 
@@ -36,12 +37,23 @@ beforeEach(async () => {
       otherFacility: {},
     }),
   )
-  await setBirthDate(store, 122)
+  await setBirthDate(store, 122, 'day')
   await setAnswer(9042, 7330) // I'm a male
+  const weightId = algorithm.config.basic_questions.weight_question_id
+  await setAnswer(weightId, '10')
 })
 
 describe('Final diagnosis are included / excluded correctly in TIMCI Senegale ', () => {
   it('Should be propose uncomplicated hernia', async () => {
-    
+    await setAnswer(9770, 8314) // Urine or genital problems
+    await setAnswer(9439, 8069) // Groin or genital pain / swelling
+
+    await setAnswer(9297, 7752) // Painful swelling of groin
+    await setAnswer(9384, 7941) // Inguinal / femoral hernia
+
+    await setAnswer(10049, 3252) // Hernia reducible
+
+    const result = SetDiagnosesService()
+    expect(result.diagnosis.proposed).toEqual(expect.arrayContaining([10047])) // Uncomplicated hernia
   })
 })
