47210ad378de4fd56d83b619f34f462f8d64f9da
batuhanfaik <batuhanfaik@gmail.com>
2022-03-01 22:01:57 +0100

    Reformat code

--- __tests__/Store/Algorithm/Load.test.js
+++ __tests__/Store/Algorithm/Load.test.js
@@ -1,18 +1,20 @@
-import {store} from "@/Store";
-import LoadAlgorithm from "@/Store/Algorithm/Load";
-import algorithmFile from "../../version_1.json";
+import { store } from '@/Store'
+import LoadAlgorithm from '@/Store/Algorithm/Load'
+import algorithmFile from '../../version_1.json'
 
 beforeAll(async () => {
-    const algorithmFile = require('../../version_1.json')
+  const algorithmFile = require('../../version_1.json')
 })
 
 describe('Load ePOCT+ Dynamic Tanzania algorithm', () => {
-    it('should load algorithm', async () => {
-        await store.dispatch(
-            LoadAlgorithm.action({
-                newAlgorithm: algorithmFile,
-            }),
-        )
-        expect(store.getState().algorithm.item.algorithm_name).toBe('ePOCT+ Dynamic Tanzania')
-    })
+  it('should load algorithm', async () => {
+    await store.dispatch(
+      LoadAlgorithm.action({
+        newAlgorithm: algorithmFile,
+      }),
+    )
+    expect(store.getState().algorithm.item.algorithm_name).toBe(
+      'ePOCT+ Dynamic Tanzania',
+    )
+  })
 })
