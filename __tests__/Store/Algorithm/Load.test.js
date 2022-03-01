5c87db3b7632f6ac91fe578fb13d5e61d875d126
batuhanfaik <batuhanfaik@gmail.com>
2022-03-01 22:02:28 +0100

    Remove unnecessary requirement

--- __tests__/Store/Algorithm/Load.test.js
+++ __tests__/Store/Algorithm/Load.test.js
@@ -2,10 +2,6 @@ import { store } from '@/Store'
 import LoadAlgorithm from '@/Store/Algorithm/Load'
 import algorithmFile from '../../version_1.json'
 
-beforeAll(async () => {
-  const algorithmFile = require('../../version_1.json')
-})
-
 describe('Load ePOCT+ Dynamic Tanzania algorithm', () => {
   it('should load algorithm', async () => {
     await store.dispatch(
