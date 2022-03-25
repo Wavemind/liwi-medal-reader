02dc3aa14acf96318d3949566bcc28b440d99a0d
Sinan Ucak <ucak.sinan@gmail.com>
2022-03-25 17:46:34 +0100

    working on it

--- src/Services/Validation/Step/Medicines.js
+++ src/Services/Validation/Step/Medicines.js
@@ -8,12 +8,15 @@ export default async errors => {
   const state = store.getState()
   const agreed = state.medicalCase.item.diagnosis.agreed
   const additional = state.medicalCase.item.diagnosis.additional
+  const custom = state.medicalCase.item.diagnosis.custom
   const isArmControl = state.algorithm.item.is_arm_control
 
   if (!isArmControl) {
     let allProposedDrugs = []
     let allAgreedDrugs = []
     let allRefusedDrugs = []
+    let allCustomDrugs = []
+    let allDrugs = []
 
     Object.values({ ...agreed, ...additional }).forEach(diagnosis => {
       allProposedDrugs = allProposedDrugs.concat(diagnosis.drugs.proposed)
@@ -21,6 +24,9 @@ export default async errors => {
         Object.values(diagnosis.drugs.agreed).map(agreedDrug => agreedDrug.id),
       )
       allRefusedDrugs = allRefusedDrugs.concat(diagnosis.drugs.refused)
+      allCustomDrugs = allCustomDrugs.concat(
+        Object.values(diagnosis.drugs.custom).map(customDrug => customDrug.id),
+      )
     })
 
     allProposedDrugs.forEach(proposedNode => {
@@ -31,6 +37,18 @@ export default async errors => {
         )
       ) {
         errors[proposedNode] = i18n.t('validation.medicines_required')
+      }
+    })
+
+    Object.values({ ...agreed, ...additional }).forEach(diagnosis => {
+      allDrugs = allDrugs
+        .concat(Object.values(diagnosis.drugs.agreed).map(drug => drug))
+        .concat(Object.values(diagnosis.drugs.additional).map(drug => drug))
+    })
+
+    allDrugs.forEach(drug => {
+      if (!drug.formulation_id) {
+        errors[drug.id] = i18n.t('validation.formulation_required')
       }
     })
   }
