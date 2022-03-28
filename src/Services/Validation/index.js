0cae58426d9c2ca07b713af8a8ce9f6f34bcddf5
Sinan Ucak <ucak.sinan@gmail.com>
2022-03-28 11:03:19 +0200

    removed unused files

--- src/Services/Validation/index.js
+++ src/Services/Validation/index.js
@@ -1,6 +1,5 @@
 export { default as RegistrationStepService } from './Step/Registration'
 export { default as FinalDiagnosesStepService } from './Step/FinalDiagnoses'
-export { default as FormulationStepService } from './Step/Formulations'
 export { default as MedicinesStepService } from './Step/Medicines'
 export { default as QuestionValidationService } from './Question'
 export { default as ValidationStepService } from './Step'
